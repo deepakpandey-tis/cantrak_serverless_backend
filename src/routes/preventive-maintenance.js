72b435ff57541ac4a3950ed688f8d6be289f39f2
Shubham Tiwari <shubham.tiwari@alcanzarsoft.com>
2019-10-05 19:04:21 +0530

    completed createPmTaskSchedule, getPmAssetListByFilter, assignTeamToPmTask, postFeedbackWithImages apis

--- src/routes/preventive-maintenance.js
+++ src/routes/preventive-maintenance.js
@@ -6,6 +6,9 @@ const authMiddleware = require('../middlewares/auth')
 const pmController = require("../controllers/preventive-maintenance")
 
 router.post('/create-pm-task-schedule', authMiddleware.isAuthenticated, authMiddleware.isAdmin, pmController.createPmTaskSchedule)
-
+router.post('/get-pm-asset-list-by-filter', authMiddleware.isAuthenticated, authMiddleware.isAdmin, pmController.getPmAssetListByFilter)
+router.post('/assign-team-to-pm-task', authMiddleware.isAuthenticated,authMiddleware.isAdmin, pmController.assignTeamToPmTask)
+router.post('/pm-post-feedback-for-asset', authMiddleware.isAuthenticated, authMiddleware.isAdmin, pmController.postFeedbackWithImages)
+router.post('/get-pm-feedback-list',authMiddleware.isAuthenticated,authMiddleware.isAdmin,pmController.getFeedbackList)
 
 module.exports = router
