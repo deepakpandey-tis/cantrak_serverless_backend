22fb37ed1a3bd01680f93819c2d24d5b10e03feb
AVDHESHKUMAR <avdhesh.kumar@alcanzarsoft.com>
2019-12-07 13:32:24 +0530

    add multiple role & project in team

--- src/controllers/administration-features/project.js
+++ src/controllers/administration-features/project.js
@@ -480,8 +480,9 @@ const ProjectController = {
   },
   getProjectAllList: async (req, res) => {
     try {
-      
-    
+     
+       let orgId  =  req.orgId
+     
       let rows = await knex("projects")
             .select([
               "projects.id as id",
