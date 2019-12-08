8f72b3c4f09fb3b91a2aa8f24fec1693ceac9bb2
Shubham Tiwari <shubham.tiwari@alcanzarsoft.com>
2019-12-08 12:54:15 +0530

    complete role management for orgUser/orgAdmin

--- src/middlewares/role.js
+++ src/middlewares/role.js
@@ -60,41 +60,76 @@ const roleMiddleware = {
         }
 
         if (req.orgUser) {
+                           const teamsResult = await knex("team_users")
+                             .select("teamId")
+                             .where({ userId, orgId });
+                           let teams = teamsResult.map(v => Number(v.teamId));
+                           console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Teams: ", teams);
+                          //  req.teams = teams;
 
-          const result = await knex("team_users")
-            .innerJoin(
-              "team_roles_project_master",
-              "team_users.teamId",
-              "team_roles_project_master.teamId"
-            )
-            .innerJoin(
-              "role_resource_master",
-              "team_roles_project_master.roleId",
-              "role_resource_master.roleId"
-            )
-            .select([
-              "team_roles_project_master.projectId as projectId",
-              "role_resource_master.resourceId as resourceId"
-            ]).where({'team_users.userId':userId,'team_users.orgId':req.orgId});
+                          //  // Now find for these teams which role has access to which project
+                          //  let rolesOnProject = [];
+                          //  for (let team of teams) {
+                          //    let result = await knex(
+                          //      "team_roles_project_master"
+                          //    )
+                          //      .select("roleId", "projectId")
+                          //      .where({ teamId: team });
+                          //    rolesOnProject.push(result);
+                          //  }
+                          //  rolesOnProject = _.flatten(rolesOnProject);
 
-            // let userProjectResources = result;
-            console.log(
-              "result***********************************************************",
-              result
-            );
-            if(result.length === 0){
-              next(createError(403))
-            }
+                          //  let projects = _.flattenDeep(
+                          //    Object.entries(
+                          //      _.groupBy(rolesOnProject, v => {
+                          //        return v.projectId;
+                          //      })
+                          //    ).map(([key, val]) => ({
+                          //      [key]: val.map(v => v.roleId)
+                          //    }))
+                          //  );
 
-            let userProjectResources = _.chain(result).groupBy("resourceId").map((value, key) => ({ id: key, projects: value.map(a => a.projectId) })).value();
-            req.userProjectResources = userProjectResources;
+                          //  let projectAcccessToResources = [];
+                          //  console.log(
+                          //    "***********************Roles on project",
+                          //    rolesOnProject
+                          //  );
+                           
 
-            console.log(
-              "userProjectResources***********************************************************",
-              userProjectResources
-            );
-        
-        }
+                           // }
+
+                           const result = await knex("team_users")
+                             .innerJoin(
+                               "team_roles_project_master",
+                               "team_users.teamId",
+                               "team_roles_project_master.teamId"
+                             )
+                             .innerJoin(
+                               "role_resource_master",
+                               "team_roles_project_master.roleId",
+                               "role_resource_master.roleId"
+                             )
+                             .select([
+                               "team_roles_project_master.projectId as projectId",
+                               "role_resource_master.resourceId as resourceId"
+                             ]).where({'team_users.userId':userId,'team_users.orgId':req.orgId}).whereIn('team_users.teamId',teams);
+
+                             console.log(
+                               "result***********************************************************",
+                               result
+                             );
+                             if(result.length === 0){
+                               next(createError(403))
+                             }
+
+                             let userProjectResources = _.chain(result).groupBy("resourceId").map((value, key) => ({ id: key, projects: value.map(a => a.projectId) })).value();
+                             req.userProjectResources = userProjectResources;
+
+                             console.log(
+                               "userProjectResources***********************************************************",
+                               userProjectResources
+                             );
+                         }
 
         let currentUser = await knex("users").where({
           id: userId,
@@ -125,3 +160,70 @@ const roleMiddleware = {
 };
 
 module.exports = roleMiddleware;
+
+
+
+
+
+
+
+
+
+/*
+
+// Find which teams this user belongs
+          const teamsResult = await knex("team_users")
+            .select("teamId")
+            .where({ userId, orgId });
+          let teams = teamsResult.map(v => v.teamId);
+          console.log("Teams: ", teams);
+          req.teams = teams;
+
+          // Now find for these teams which role has access to which project
+          let rolesOnProject = [];
+          for (let team of teams) {
+            let result = await knex("team_roles_project_master")
+              .select("roleId", "projectId")
+              .where({ teamId: team });
+            rolesOnProject.push(result);
+          }
+          rolesOnProject = _.flatten(rolesOnProject);
+
+          let projects = _.flattenDeep(
+            Object.entries(
+              _.groupBy(rolesOnProject, v => {
+                return v.projectId;
+              })
+            ).map(([key, val]) => ({
+              [key]: val.map(v => v.roleId)
+            }))
+          );
+
+          let projectAcccessToResources = [];
+          for (let project of projects) {
+            console.log("Single Project: ", project);
+            let key = _.flatten(_.keys(project));
+            let roles = _.flatten(
+              _.entries(project).map(([key, v]) => {
+                return v;
+              })
+            );
+
+            for (let role of roles) {
+              // Get all resources for each role
+              let resourcesResult = await knex("role_resource_master")
+                .select("resourceId")
+                .where({ roleId: role });
+                resourcesResult.forEach(v => {
+                  projectAcccessToResources.push({id:v.resourceId,projects:keys})
+                })
+//                projectAcccessToResources.push()
+              // projectAcccessToResources[key] = resourcesResult.map(v =>
+              //   Number(v.resourceId)
+              // );
+            }
+          }
+
+
+
+*/
--- src/routes/administration-features/task-group.js
+++ src/routes/administration-features/task-group.js
@@ -3,28 +3,59 @@ const { Router } = require("express")
 const router = Router()
 const authMiddleware  = require('../../middlewares/auth')
 const roleMiddleware  = require('../../middlewares/role')
+const resourceAccessMiddleware  = require('../../middlewares/resourceAccessMiddleware')
 const taskGroupController = require('../../controllers/administration-features/task-group')
 
 // CREATE TASK GROUP TEMPLATE
-router.post('/create-task-group-template', authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission, taskGroupController.createTaskGroupTemplate)
+router.post(
+  "/create-task-group-template",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.createTaskGroupTemplate
+);
  
 // GET LIST TASK TEMPLATE
-router.post('/get-group-template-list',authMiddleware.isAuthenticated,
-  roleMiddleware.parseUserPermission, taskGroupController.getGroupTemplateList)
+router.post(
+  "/get-group-template-list",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.getGroupTemplateList
+);
 
 // GET GROUP TASK LIST
-router.post('/get-group-task-list',authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission, taskGroupController.getGroupTaskList)
+router.post(
+  "/get-group-task-list",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.getGroupTaskList
+);
 
 //CREATE PM TASK GROUP SCHEDULE
-router.post('/create-pm-taskgroup-schedule',authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission, taskGroupController.createPmTaskgroupSchedule)
+router.post(
+  "/create-pm-taskgroup-schedule",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.createPmTaskgroupSchedule
+);
 
 // GET TASK GROUP SCHEDULE LIST
-router.post('/get-taskgroup-schedule-list',authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission, taskGroupController.getTaskGroupScheduleList)
+router.post(
+  "/get-taskgroup-schedule-list",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.getTaskGroupScheduleList
+);
 
 router.post(
   "/get-pm-list",
   authMiddleware.isAuthenticated,
   roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
   taskGroupController.getPmList
 );
 
@@ -33,38 +64,142 @@ router.post(
   "/create-brand-new-pm",
   authMiddleware.isAuthenticated,
   roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+
   taskGroupController.createBrandNewPm
 );
 
 // GET TASK GROUP ASSET PMS LIST
-router.post('/get-task-group-asset-pms-list',authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission,taskGroupController.getTaskGroupAssetPmsList)
-router.post('/get-task-group-asset-pms-list-on-today',authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission,taskGroupController.getTaskGroupAssetPmsListOnToday)
+router.post(
+  "/get-task-group-asset-pms-list",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.getTaskGroupAssetPmsList
+);
+router.post(
+  "/get-task-group-asset-pms-list-on-today",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.getTaskGroupAssetPmsListOnToday
+);
 
 // CREATE TASK TEMPLATE
-router.post('/create-task-template',authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission,taskGroupController.createTaskTemplate)
+router.post(
+  "/create-task-template",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.createTaskTemplate
+);
 // GET TASK TEMPLATE LIST
-router.post('/get-task-template-list',authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission,taskGroupController.getTaskTemplateList)
+router.post(
+  "/get-task-template-list",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.getTaskTemplateList
+);
 
 // GET TASK TEMPLATE COMPLETE DATA
-router.post('/get-task-template-complete-list',authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission,taskGroupController.getTaskTemplateComplateList)
+router.post(
+  "/get-task-template-complete-list",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.getTaskTemplateComplateList
+);
 
 // GET TASK GROUP ASSET PM DETAILS
-router.post('/get-taskgroup-asset-pm-details',authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission,taskGroupController.getTaskgroupAssetPmDetails)
+router.post(
+  "/get-taskgroup-asset-pm-details",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.getTaskgroupAssetPmDetails
+);
 
 // GET PM TASK DETAILS
-router.post('/get-pm-task-details',authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission,taskGroupController.getPmTaskDetails)
+router.post(
+  "/get-pm-task-details",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.getPmTaskDetails
+);
 
-router.post('/create-pm-template',authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission,taskGroupController.createPMTemplate)
-router.post('/update-task-status',authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission,taskGroupController.updateTaskStatus)
-router.post('/send-feedback-for-task',authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission,taskGroupController.sendFeedbackForTask)
-router.post('/edit-work-order',authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission,taskGroupController.editWorkOrder)
-router.post('/get-task-group-details',authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission,taskGroupController.getTaskGroupDetails)
-router.get('/get-task-group-template-list',authMiddleware.isAuthenticated,roleMiddleware.parseUserPermission,taskGroupController.getTaskGroupTemplateList)
-router.post('/update-task-group-template-detail', authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, taskGroupController.updateTaskGroupDetails)
-router.post('/get-task-feedbacks', authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, taskGroupController.getFeedbacksOfTask)
+router.post(
+  "/create-pm-template",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.createPMTemplate
+);
+router.post(
+  "/update-task-status",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.updateTaskStatus
+);
+router.post(
+  "/send-feedback-for-task",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.sendFeedbackForTask
+);
+router.post(
+  "/edit-work-order",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.editWorkOrder
+);
+router.post(
+  "/get-task-group-details",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.getTaskGroupDetails
+);
+router.get(
+  "/get-task-group-template-list",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.getTaskGroupTemplateList
+);
+router.post(
+  "/update-task-group-template-detail",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.updateTaskGroupDetails
+);
+router.post(
+  "/get-task-feedbacks",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.getFeedbacksOfTask
+);
 
-router.post('/get-task-group-schedule-details', authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, taskGroupController.getTaskGroupScheduleDetails)
-router.post('/update-old-task-group-schedule-with-work-orders', authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, taskGroupController.updateOldTaskGroupScheduleWithWorkOrders)
+router.post(
+  "/get-task-group-schedule-details",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.getTaskGroupScheduleDetails
+);
+router.post(
+  "/update-old-task-group-schedule-with-work-orders",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  taskGroupController.updateOldTaskGroupScheduleWithWorkOrders
+);
 
 
 module.exports = router
