0a8a58755b75f83d58aadcc52cf893a6a6cfe918
AVDHESHKUMAR <avdhesh.kumar@alcanzarsoft.com>
2019-12-11 13:17:43 +0530

    apis

--- src/controllers/administration-features/company.js
+++ src/controllers/administration-features/company.js
@@ -331,7 +331,7 @@ const companyController = {
 
      let tempraryDirectory = null;
      // if (process.env.dev && process.env.dev) {
-        tempraryDirectory = '/tmp';//path.join(__dirname, `/tmp`);
+        tempraryDirectory = '/tmp/';//path.join(__dirname, `/tmp`);
       //} else {
         //tempraryDirectory = 'tmp/';
       //}
