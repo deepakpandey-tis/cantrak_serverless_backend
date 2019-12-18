a14e29df16a27497ccb9ec573c3f6c9bbf250ded
AVDHESHKUMAR <avdhesh.kumar@alcanzarsoft.com>
2019-12-18 19:21:43 +0530

    import apis in status property type other

--- src/routes/administration-features/status.js
+++ src/routes/administration-features/status.js
@@ -16,6 +16,30 @@ router.post('/delete-status', authMiddleware.isAuthenticated,  statusController.
 router.get('/export-status', authMiddleware.isAuthenticated,  statusController.exportStatus);
 router.post('/status-details', authMiddleware.isAuthenticated,  statusController.statusDetails);
 
+/**IMPORT STATUS DATA */
+const path       = require('path');
+let tempraryDirectory = null;
+        if (process.env.IS_OFFLINE) {
+           tempraryDirectory = 'tmp/';
+         } else {
+           tempraryDirectory = '/tmp/';  
+         }
+var multer  = require('multer');
+var storage = multer.diskStorage({
+	destination: tempraryDirectory,
+	filename: function ( req, file, cb ) {
+        let ext =  path.extname(file.originalname)
+        if(ext=='.csv'){
+        time = Date.now();
+        cb( null, 'ServiceStatusData-'+time+ext);
+        }else{
+            return false
+        }
+	}
+});
+var upload = multer( { storage: storage } );
+router.post('/import-service-status-data',upload.single('file'), authMiddleware.isAuthenticated, statusController.importServiceStatusData)
+
 
 module.exports = router;
  
