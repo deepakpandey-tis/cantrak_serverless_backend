a57ae2cfaf11ef49fc5a2cf434474f6464ce1f34
Shubham Tiwari <shubham.tiwari@alcanzarsoft.com>
2020-02-12 13:28:41 +0530

    facility booking api modification

--- src/controllers/facility_booking.js
+++ src/controllers/facility_booking.js
@@ -1,7 +1,18 @@
-const facilityBookingController = {
-    test: async(req,res) => {
-        try {
+const knex = require("../db/knex");
+const Joi = require("@hapi/joi");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const moment = require("moment");
 
+const uuidv4 = require("uuid/v4");
+var jwt = require("jsonwebtoken");
+const _ = require("lodash");
+
+const facilityBookingController = {
+    addFacility: async(req,res) => {
+        try {
+            const payload = req.body;
+            // const schem
         } catch(err) {
 
         }
--- src/routes/facility_booking.js
+++ src/routes/facility_booking.js
@@ -1,8 +1,12 @@
 const Router = require('express').Router;
 const authMiddleware = require('../middlewares/auth')
+const roleMiddleware = require('../middlewares/role')
+const resourceAccessMiddleware = require('../middlewares/resourceAccessMiddleware')
 const facilityBookingController = require('../controllers/facility_booking')
 const router = Router()
 
-router.get('/', authMiddleware.isAuthenticated,facilityBookingController.test)
+router.get('/', authMiddleware.isAuthenticated, 
+roleMiddleware.parseUserPermission,
+facilityBookingController.test)
 
 module.exports = router;
