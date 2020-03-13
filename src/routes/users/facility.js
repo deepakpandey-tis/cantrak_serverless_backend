e19773bdd66120ff00b9ba8ac4f8346c7c0b6822
AVDHESHKUMAR <avdhesh.kumar@alcanzarsoft.com>
2020-03-13 18:23:24 +0530

    remove middleware

--- src/routes/users/facility.js
+++ src/routes/users/facility.js
@@ -10,23 +10,21 @@ const router = Router()
 
 /* USER FACILITY LIST */
 router.post('/get-user-facility-list', authMiddleware.isAuthenticated,
-    roleMiddleware.parseUserPermission,
     facilityBookingController.getUserFacilityList);
 
 /* FACILITY DETAILS */
 router.post('/user-facility-details', authMiddleware.isAuthenticated,
-    roleMiddleware.parseUserPermission,
+    
     facilityBookingController.userFacilityDetails);
 
 /* YOUR  FACILITY BOOKING LIST */
 router.post('/your-facility-booking-list', authMiddleware.isAuthenticated,
-    roleMiddleware.parseUserPermission,
+    
     facilityBookingController.yourFacilityBookingList);
 
 
 /*FACILITY BOOK NOW */
 router.post('/facility-book-now', authMiddleware.isAuthenticated,
-    roleMiddleware.parseUserPermission,
     facilityBookingController.facilityBookNow);
 
 
