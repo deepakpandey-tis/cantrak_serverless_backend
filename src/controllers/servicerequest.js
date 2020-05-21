ff6282dd6fdaf6eb5d028748ebc932febc99ec3c
AVDHESHKUMAR <avdhesh.kumar@alcanzarsoft.com>
2020-05-21 08:58:16 +0530

    change service request filter field

--- src/controllers/servicerequest.js
+++ src/controllers/servicerequest.js
@@ -845,7 +845,7 @@ const serviceRequestController = {
             .where(qb => {
 
               if (serviceId) {
-                qb.where({ 'service_requests.id': serviceId })
+                qb.where({ 'service_requests.displayId': serviceId })
               }
 
               if (location) {
@@ -988,7 +988,7 @@ const serviceRequestController = {
             .where(qb => {
 
               if (serviceId) {
-                qb.where({ 'service_requests.id': serviceId })
+                qb.where({ 'service_requests.displayId': serviceId })
               }
 
               if (location) {
