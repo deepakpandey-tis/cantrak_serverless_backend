87fdd27afd117cacf05ad10c42840bb5ffb6fc3f
Amar Pandey <amar@infiniseasoft.com>
2020-05-24 23:19:45 +0530

    add courier

--- src/controllers/administration-features/courier-storage.js
+++ src/controllers/administration-features/courier-storage.js
@@ -15,6 +15,95 @@ const request = require("request");
 const path = require("path");
 
 const courierStorageController = {
+  addCourier: async (req, res) => {
+    try {
+      let courier = null;
+      let userId = req.me.id;
+      let orgId = req.orgId;
+
+      await knex.transaction(async (trx) => {
+        const courierPayLoad = req.body;
+        console.log("[Controllers][Courier][add]", courierPayLoad);
+
+        const schema = Joi.object().keys({
+          courierCode: Joi.string().required(),
+          courierName: Joi.string().required(),
+          mobileNo: Joi.string().required(),
+          website: Joi.string().optional(),
+          address: Joi.string().required(),
+        });
+        const result = Joi.validate(courierPayLoad, schema);
+        console.log("[Controller][Courier][add]:Joi result", result);
+        if (result && result.hasOwnProperty("error") && result.error) {
+          return res.status(400).json({
+            errors: [
+              { code: "VALIDATION_ERROR", message: result.error.message },
+            ],
+          });
+        }
+        const existCourierCode = await knex("courier").where({
+          courierCode: courierPayLoad.courierCode.toUpperCase(),
+          orgId: orgId,
+        });
+        console.log(
+          "[controllers][courier][add]: courierCode",
+          existCourierCode
+        );
+        if (existCourierCode && existCourierCode.length) {
+          return res.status(400).json({
+            errors: [
+              {
+                code: "TYPE_CODE_EXIST_ERROR",
+                message: "Courier Code already exist !",
+              },
+            ],
+          });
+        }
+        const currentTime = new Date().getTime();
+
+        const insertData = {
+          ...courierPayLoad,
+          orgId: orgId,
+          createdBy: userId,
+          courierCode: courierPayLoad.courierCode.toUpperCase(),
+          createdAt: currentTime,
+          updatedAt: currentTime,
+        };
+        console.log("[controllers][courier][add]: Insert Data", insertData);
+
+        const incidentResult = await knex
+          .insert(insertData)
+          .returning(["*"])
+          .transacting(trx)
+          .into("courier");
+
+          incident = incidentResult[0]
+
+          trx.commit;
+      });
+
+      res.status(200).json({
+        data: {
+          category: incident
+        },
+        message: "Courier added successfully !"
+      });
+    } catch (err) {
+      console.log("[controllers][courier][courierAdd] :  Error", err);
+
+      res.status(500).json({
+        errors: [{ code: "UNKNOWN_SERVER_ERROR", message: err.message }]
+      });
+
+    }
+  },
+
+  // addStorage:async(req,res)={
+  //   try{
+
+  //   }catch(err){}
+  // },
+
   getCourierList: async (req, res) => {
     try {
       let sortPayLoad = req.body;
--- src/routes/administration-features/courier-storage.js
+++ src/routes/administration-features/courier-storage.js
@@ -1,6 +1,17 @@
-const Router = require('express').Router
-const router = Router()
+const Router = require("express").Router;
+const router = Router();
 
-const authMiddleware = require('../../middlewares/auth')
-const courierStorageController = require('../../controllers/administration-features/courier-storage')
-router.post('/get-courier-list',authMiddleware.isAuthenticated,courierStorageController.getCourierList)
+const authMiddleware = require('../../middlewares/auth');
+const courierStorageController = require('../../controllers/administration-features/courier-storage');
+router.post(
+  '/add-courier',
+  authMiddleware.isAuthenticated,
+  courierStorageController.addCourier
+);
+router.post(
+  '/get-courier-list',
+  authMiddleware.isAuthenticated,
+  courierStorageController.getCourierList
+);
+
+module.exports = router;
