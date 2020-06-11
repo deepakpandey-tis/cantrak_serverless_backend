fbe000e66997d91075c51e344f0c33fdec332ea2
Amar Pandey <amar@infiniseasoft.com>
2020-06-11 10:53:54 +0530

    api for cancelled facilities list

--- src/controllers/facility_booking.js
+++ src/controllers/facility_booking.js
@@ -3469,6 +3469,57 @@ const facilityBookingController = {
     }
 
   },
+  getFacilityBookedCancelledList:async(req,res)=>{
+    try{
+      let payload = req.body
+      let {startDate,endDate}= req.body
+
+      if (startDate && endDate) {
+        startNewDate = moment(startDate).startOf("time").format();
+        endNewDate = moment(endDate).endOf("time").format();
+        startTime = new Date(startNewDate).getTime();
+        endTime = new Date(endNewDate).getTime();
+      }
+      const schema = Joi.object().keys({
+        startDate: Joi.string().required(),
+        endDate: Joi.string().required()
+      });
+
+      const result = Joi.validate(payload, schema);
+      if (result && result.hasOwnProperty("error") && result.error) {
+        return res.status(400).json({
+          errors: [{ code: "VALIDATION_ERROR", message: result.error.message }],
+        });
+      }
+      let listResult = await knex("entity_bookings")
+      .leftJoin(
+        "facility_master",
+        "entity_bookings.entityId",
+        "facility_master.id"
+      )
+      .leftJoin("users", "entity_bookings.bookedBy", "users.id")
+      .select([
+        "entity_bookings.*",
+        "facility_master.name",
+        "users.name as bookedUser",
+      ])
+      .where("entity_bookings.bookingStartDateTime", ">=", startTime)
+      .where("entity_bookings.bookingEndDateTime", "<=", endTime)
+      .where("entity_bookings.isBookingCancelled", "=", true)
+      ;
+      return res.status(200).json({
+        data: {
+          booking: listResult,
+        },
+        message: "Facility booked List!",
+      });
+
+    }catch(err){
+      res.status(500).json({
+        errors: [{ code: "UNKNOWN_SERVER_ERRROR", message: err.message }],
+      });
+    }
+  },
   getFacilityBookedListbydate: async (req, res) => {
     try {
       let payload = req.body;
