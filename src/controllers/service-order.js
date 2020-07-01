049cf6bef05550750133b89dd155521e39780dd7
root <deepak.twr@gitlab.com>
2020-07-01 18:17:33 +0530

    service order wrong data updated correct

--- src/controllers/service-order.js
+++ src/controllers/service-order.js
@@ -2683,15 +2683,28 @@ const serviceOrderController = {
                 .leftJoin('incident_sub_categories', 'service_problems.problemId', 'incident_sub_categories.id')
                 .leftJoin('incident_type', 'incident_sub_categories.incidentTypeId', 'incident_type.id')
                 .select([
+                    'service_problems.serviceRequestId',
                     'service_problems.problemId',
                     'service_problems.categoryId',
+                    'service_problems.serviceRequestId',
                     'incident_type.typeCode as problemTypeCode',
                     'incident_type.descriptionEng as problemType',
                     'incident_categories.categoryCode',
                     'incident_categories.descriptionEng as category',
                     'incident_sub_categories.descriptionEng as subCategory',
                     'service_requests.serviceStatusCode',
-                ])
+                ])   
+                .groupBy([
+                    "service_problems.problemId",
+                    "service_problems.categoryId",
+                    "service_problems.serviceRequestId",
+                    "incident_type.descriptionEng",
+                    "incident_type.typeCode",
+                    "incident_categories.descriptionEng",
+                    "incident_sub_categories.descriptionEng",
+                    "service_requests.serviceStatusCode",
+                    "incident_categories.categoryCode"
+                ])             
                 .whereIn('service_problems.serviceRequestId', serviceIds)
                 .where({ 'service_problems.orgId': req.orgId })
                 .orderBy('incident_type.typeCode', 'asc');
