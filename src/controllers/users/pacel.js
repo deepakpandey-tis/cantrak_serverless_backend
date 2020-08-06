8b33e9ce9e1bb09364faef5b49d36f06a2f5740a
root <deepak.twr@gitlab.com>
2020-08-07 01:35:19 +0530

    parcel apis updated

---
+++ src/controllers/users/pacel.js
@@ -0,0 +1,103 @@
+const knex = require("../../db/knex");
+const Joi = require("@hapi/joi");
+const moment = require("moment-timezone");
+// const momentWithTZ = require("moment");
+const _ = require("lodash");
+const emailHelper = require('../../helpers/email')
+
+
+var arrayCompare = require("array-compare");
+
+const parcelController = {
+
+    /*GET USER FACILITY LIST */
+    getParcelList: async (req, res) => {
+
+        try {
+            let id = req.me.id;
+            let propertyUnitFinalResult = null;
+            //let resourceProject = req.userProjectResources[0].projects;
+            let { listType } = req.body;
+            let resultData;
+
+            console.log("customerHouseInfo", req.me.houseIds);
+            let houseIdArray = req.me.houseIds;
+
+            propertyUnitFinalResult = await knex.from('property_units')
+                .where({ orgId: req.orgId })
+                .whereIn('id', houseIdArray);
+
+            let projectArray = _.uniqBy(propertyUnitFinalResult, 'projectId').map(v => v.projectId)
+
+
+            resultData = await knex.from('parcel_management')
+                .leftJoin(
+                    "parcel_user_tis",
+                    "parcel_management.id",
+                    "parcel_user_tis.parcelId"
+                )
+                .leftJoin(
+                    "parcel_user_non_tis",
+                    "parcel_management.id",
+                    "parcel_user_non_tis.parcelId"
+                )
+                .leftJoin(
+                    "property_units",
+                    "parcel_user_tis.unitId",
+                    "property_units.id"
+                )
+                .leftJoin("users", "parcel_user_tis.tenantId", "users.id")
+                .select([
+                    "parcel_management.id",
+                    "parcel_user_tis.unitId",
+                    "parcel_management.trackingNumber",
+                    "parcel_management.parcelStatus",
+                    "users.name as tenant",
+                    "parcel_management.createdAt",
+                    "parcel_management.pickedUpType",
+                ])
+
+                .where({ 'parcel_management.orgId': req.orgId, 'parcel_management.moderationStatus': true, 'parcel_management.status': true })
+                .whereIn('parcel_management.projectId', projectArray)
+                .orderBy('parcel_management.id', 'desc')
+                .groupBy('parcel_management.id', 'companies.id', 'projects.id', 'buildings_and_phases.id', 'floor_and_zones.id')
+                .distinct('parcel_management.id')
+
+
+
+            const Parallel = require('async-parallel');
+            resultData = await Parallel.map(resultData, async pd => {
+
+                let imageResult = await knex.from('images').select('s3Url', 'title', 'name')
+                    .where({ "entityId": pd.id, "entityType": 'facility_master' })
+
+                return {
+                    ...pd,
+                    uploadedImages: imageResult,                 
+                    sortBy
+                }
+
+            })
+
+            res.status(200).json({
+                data: {
+                    facilityData: _.orderBy(resultData, 'sortBy', 'desc')
+                },
+                message: "Facility list successfully!"
+            })
+
+        } catch (err) {
+
+            return res.status(500).json({
+                errors: [{ code: "UNKNOWN_SERVER_ERROR", message: err.message }]
+            });
+
+        }
+
+    },
+
+}
+
+
+
+module.exports = parcelController;
