3b1a37a8db1e7702899ed451cd85e57017796941
Amar Pandey <amar@infiniseasoft.com>
2020-08-08 18:10:17 +0530

    parcel notification

--- src/controllers/parcel-management.js
+++ src/controllers/parcel-management.js
@@ -147,7 +147,6 @@ const parcelManagementController = {
 
         let noOrgUserDataPayload = req.body.non_org_user_data;
 
-        // console.log("noOrgUserDataPayload",noOrgUserDataPayload)
         noOrgUserData = await knex("parcel_user_non_tis")
           .insert({
             parcelId: parcelResult.id,
@@ -217,7 +216,7 @@ const parcelManagementController = {
         data: parcelResult,
         noOrgUserData: noOrgUserData,
         orgUserData: orgUserData,
-        message: "Parcel added",
+        message: "Parcel added successfully !",
       });
     } catch (err) {
       console.log("[controllers][parcel_management][addParcel] :  Error", err);
@@ -438,224 +437,14 @@ const parcelManagementController = {
       });
     }
   },
-  // getPendingParcelList: async (req, res) => {
-  //   try {
-  //     let reqData = req.query;
-  //     console.log("requested data", reqData);
-  //     let total, rows;
-
-  //     let pagination = {};
-  //     let per_page = reqData.per_page || 10;
-  //     let page = reqData.current_page || 1;
-  //     if (page < 1) page = 1;
-  //     let offset = (page - 1) * per_page;
-  //     let filters = {};
-
-  //     let { unitId, trackingNumber, tenant, status } = req.body;
-
-  //     console.log("request body", req.body);
-  //     if (unitId || trackingNumber || tenant) {
-  //       try {
-  //         [total, rows] = await Promise.all([
-  //           knex
-  //             .count("* as count")
-  //             .from("parcel_management")
-  //             .leftJoin(
-  //               "parcel_user_tis",
-  //               "parcel_management.id",
-  //               "parcel_user_tis.parcelId"
-  //             )
-  //             .leftJoin(
-  //               "parcel_user_non_tis",
-  //               "parcel_management.id",
-  //               "parcel_user_non_tis.parcelId"
-  //             )
-  //             .leftJoin(
-  //               "property_units",
-  //               "parcel_user_tis.unitId",
-  //               "property_units.id"
-  //             )
-  //             .leftJoin("users", "parcel_user_tis.tenantId", "users.id")
-  //             .where("parcel_management.orgId", req.orgId)
-  //             .where("parcel_management.parcelStatus", 1)
-  //             .where((qb) => {
-  //               if (unitId) {
-  //                 qb.where("property_units.id", unitId);
-  //               }
-  //               if (trackingNumber) {
-  //                 qb.where("parcel_management.trackingNumber", trackingNumber);
-  //               }
-  //               if (tenant) {
-  //                 qb.where("users.id", tenant);
-  //               }
-  //               if (status) {
-  //                 qb.where("parcel_management.parcelStatus", status);
-  //               }
-  //             })
-  //             .groupBy([
-  //               "parcel_management.id",
-  //               "property_units.id",
-  //               "users.id",
-  //               "parcel_user_tis.unitId",
-  //             ]),
-  //           knex
-  //             .from("parcel_management")
-  //             .leftJoin(
-  //               "parcel_user_tis",
-  //               "parcel_management.id",
-  //               "parcel_user_tis.parcelId"
-  //             )
-  //             .leftJoin(
-  //               "parcel_user_non_tis",
-  //               "parcel_management.id",
-  //               "parcel_user_non_tis.parcelId"
-  //             )
-  //             .leftJoin(
-  //               "property_units",
-  //               "parcel_user_tis.unitId",
-  //               "property_units.id"
-  //             )
-  //             .leftJoin("users", "parcel_user_tis.tenantId", "users.id")
-  //             .select([
-  //               "parcel_management.id",
-  //               "parcel_user_tis.unitId",
-  //               "parcel_management.trackingNumber",
-  //               "parcel_management.parcelStatus",
-  //               "users.name as tenant",
-  //               "parcel_management.createdAt",
-  //               "parcel_management.pickedUpType",
-  //             ])
-  //             .where("parcel_management.orgId", req.orgId)
-  //             .where("parcel_management.parcelStatus", 1)
-  //             .where((qb) => {
-  //               if (unitId) {
-  //                 qb.where("property_units.id", unitId);
-  //               }
-  //               if (trackingNumber) {
-  //                 qb.where("parcel_management.trackingNumber", trackingNumber);
-  //               }
-  //               if (tenant) {
-  //                 qb.where("users.id", tenant);
-  //               }
-  //               if (status) {
-  //                 qb.where("parcel_management.parcelStatus", status);
-  //               }
-  //             })
-  //             .orderBy("parcel_management.id", "asc")
-  //             .offset(offset)
-  //             .limit(per_page),
-  //         ]);
-
-  //         let count = total.length;
-
-  //         pagination.total = count;
-  //         pagination.per_page = per_page;
-  //         pagination.offset = offset;
-  //         pagination.to = offset + rows.length;
-  //         pagination.last_page = Math.ceil(count / per_page);
-  //         pagination.current_page = page;
-  //         pagination.from = offset;
-  //         pagination.data = rows;
-  //       } catch (err) {
-  //         console.log(err);
-  //       }
-  //     } else {
-  //       [total, rows] = await Promise.all([
-  //         knex
-  //           .count("* as count")
-  //           .from("parcel_management")
-  //           .leftJoin(
-  //             "parcel_user_tis",
-  //             "parcel_management.id",
-  //             "parcel_user_tis.parcelId"
-  //           )
-  //           .leftJoin(
-  //             "parcel_user_non_tis",
-  //             "parcel_management.id",
-  //             "parcel_user_non_tis.parcelId"
-  //           )
-  //           .leftJoin(
-  //             "property_units",
-  //             "parcel_user_tis.unitId",
-  //             "property_units.id"
-  //           )
-  //           .leftJoin("users", "parcel_user_tis.tenantId", "users.id")
-  //           .where("parcel_management.orgId", req.orgId)
-  //           .where("parcel_management.parcelStatus", 1)
-  //           .groupBy(["parcel_management.id", "property_units.id", "users.id"]),
-  //         knex
-  //           .from("parcel_management")
-  //           .leftJoin(
-  //             "parcel_user_tis",
-  //             "parcel_management.id",
-  //             "parcel_user_tis.parcelId"
-  //           )
-  //           .leftJoin(
-  //             "parcel_user_non_tis",
-  //             "parcel_management.id",
-  //             "parcel_user_non_tis.parcelId"
-  //           )
-  //           .leftJoin(
-  //             "property_units",
-  //             "parcel_user_tis.unitId",
-  //             "property_units.id"
-  //           )
-  //           .leftJoin("users", "parcel_user_tis.tenantId", "users.id")
-  //           .select([
-  //             "parcel_management.id",
-  //             "parcel_user_tis.unitId",
-  //             "parcel_management.trackingNumber",
-  //             "parcel_management.parcelStatus",
-  //             "users.name as tenant",
-  //             "parcel_management.createdAt",
-  //             "parcel_management.pickedUpType",
-  //           ])
-  //           .where("parcel_management.orgId", req.orgId)
-  //           .where("parcel_management.parcelStatus", 1)
-  //           .groupBy([
-  //             "parcel_management.id",
-  //             "property_units.id",
-  //             "users.id",
-  //             "parcel_user_tis.unitId",
-  //           ])
-  //           .orderBy("parcel_management.id", "asc")
-  //           .offset(offset)
-  //           .limit(per_page),
-  //       ]);
-
-  //       let count = total.length;
-
-  //       pagination.total = count;
-  //       pagination.per_page = per_page;
-  //       pagination.offset = offset;
-  //       pagination.to = offset + rows.length;
-  //       pagination.last_page = Math.ceil(count / per_page);
-  //       pagination.current_page = page;
-  //       pagination.from = offset;
-  //       pagination.data = rows;
-  //     }
-
-  //     return res.status(200).json({
-  //       data: {
-  //         parcel: pagination,
-  //       },
-  //       message: "parcel List!",
-  //     });
-  //   } catch (err) {
-  //     console.log("[controllers][parcel_management][list] :  Error", err);
-  //     return res.status(500).json({
-  //       errors: [{ code: "UNKNOWN_SERVER_ERROR", message: err.message }],
-  //     });
-  //   }
-  // },
-
+ 
   getPendingParcelList: async (req, res) => {
     try {
       let payload = req.body;
       let parcelList;
-      let { unitId, tenantId, buildingPhaseId, trackingNumber } = req.body;
-      console.log("parcel payload",unitId,tenantId,buildingPhaseId,trackingNumber)
-      if (unitId || tenantId || buildingPhaseId || trackingNumber) {
+      let { unitId, tenantId, buildingPhaseId, trackingNumber,qrCode } = req.body;
+      console.log("parcel payload",unitId,tenantId,buildingPhaseId,trackingNumber,qrCode)
+      if (unitId || tenantId || buildingPhaseId || trackingNumber || qrCode) {
         try {
           parcelList = await knex
             .from("parcel_management")
@@ -705,6 +494,9 @@ const parcelManagementController = {
               }
               if (buildingPhaseId) {
                 qb.where("parcel_user_tis.buildingPhaseId", buildingPhaseId);
+              }
+              if(qrCode){
+                qb.where("parcel_management.qrCode",qrCode)
               }
             })
             .groupBy([
@@ -885,8 +677,20 @@ const parcelManagementController = {
       let parcel = null;
       let insertedImages = [];
 
+      let parcelResult = null;
+      let noOrgUserData = [];
+      let orgUserData = [];
+      // let qrUpdateResult ;
+      let images = [];
+      let orgId = req.orgId;
       await knex.transaction(async (trx) => {
-        let parcelPayload = req.body;
+        const payload = req.body;
+
+        parcelPayload = _.omit(payload,"image","id",
+        "img_url",
+        "non_org_user_data",
+        "org_user_data")
+
         const schema = Joi.object().keys({
           pickedUpType: Joi.string().required(),
           trackingNumber: Joi.string().required(),
@@ -897,9 +701,90 @@ const parcelManagementController = {
           parcelStatus: Joi.number().required(),
           parcelPriority: Joi.number().required(),
         });
+        const result = Joi.validate(parcelPayload, schema);
+        console.log("result", result);
+
+        if (result && result.hasOwnProperty("error") && result.error) {
+          return res.status(400).json({
+            errors: [
+              { code: "VALIDATION_ERROR", message: result.error.message },
+            ],
+          });
+        }
         let currentTime = new Date().getTime();
+
+        const insertData = {
+          ...parcelPayload,
+          orgId: orgId,
+          // createdBy: req.me.id,
+          // createdAt: currentTime,
+          updatedAt: currentTime,
+        };
+
+        let updateResult = await knex
+        .update(insertData)
+        .where({id:payload.id})
+        .returning(["*"])
+        .transacting(trx)
+        .into("parcel_management");
+      // console.log("add parcel result", addResult);
+      parcelResult = updateResult[0]
+
+      let noOrgUserDataPayload = req.body.non_org_user_data;
+
+      noOrgUserData = await knex("parcel_user_non_tis")
+        .update({
+          ...noOrgUserDataPayload,
+          updatedAt: currentTime,
+          
+        })
+        .where({parcelId:payload.id})
+        .returning(["*"]);
+
+      let orgUserDataPayload = req.body.org_user_data;
+
+      orgUserData = await knex("parcel_user_tis").update({
+        ...orgUserDataPayload,
+        updatedAt: currentTime,
+      })
+      .where({parcelId:payload.id})
+      .returning(['*'])
+
+      let imagesData = req.body.image;
+      console.log("imagesData", imagesData);
+      if (imagesData && imagesData.length > 0) {
+        for (let image of imagesData) {
+          let d = await knex("images")
+            .update({
+              s3Url: image.s3Url,
+              name: image.filename,
+              title: image.title,
+              updatedAt: currentTime,
+            })
+            .where({entityType:parcel_management,entityId:payload.id})
+            .returning(["*"]);
+          images.push(d[0]);
+        }
+      }
+
+      trx.commit;
+
+
       });
-    } catch (err) {}
+
+      res.status(200).json({
+        data: parcelResult,
+        noOrgUserData: noOrgUserData,
+        orgUserData: orgUserData,
+        message: "Parcel updated Successfully !",
+      });
+    } catch (err) {
+      console.log("[controllers][parcel_management][updateParcel] :  Error", err);
+      //trx.rollback
+      res.status(500).json({
+        errors: [{ code: "UNKNOWN_SERVER_ERROR", message: err.message }],
+      });
+    }
   },
   getTrackingNumberList: async (req, res) => {
     try {
