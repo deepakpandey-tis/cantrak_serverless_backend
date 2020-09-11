b324b83a4eff031d91c2a05f91083de71da22ece
Amar Pandey <amar@infiniseasoft.com>
2020-09-11 13:45:02 +0530

    outgoing parcel notification

---
+++ src/notifications/parcel/parcel-collected-notification.js
@@ -0,0 +1,135 @@
+const _ = require('lodash');
+const notification = require('../core/notification');
+
+const ALLOWED_CHANNELS = ['IN_APP', 'EMAIL', 'WEB_PUSH', 'SOCKET_NOTIFY', 'LINE_NOTIFY', 'SMS'];
+const SHOULD_QUEUE = process.env.IS_OFFLINE ? false : true;
+
+const parcelCollectedNotification = {
+    send: async (sender, receiver, data, allowedChannels = ALLOWED_CHANNELS) => {
+        try {
+
+            // console.log('[notifications][test][test-notification][send]: Sender:', sender);
+            // console.log('[notifications][test][test-notification][send]: Receiver:', receiver);
+            console.log('[notifications][collected][collected-notification][send]: Data:', data);
+
+            if (SHOULD_QUEUE) {
+                await notification.queue(sender, receiver, JSON.parse(JSON.stringify(data)), allowedChannels, __filename);
+                console.log('[notifications][collected][collected-notification][send]: All Notifications Queued');
+            } else {
+                await notification.send(sender, receiver, JSON.parse(JSON.stringify(data)), allowedChannels, parcelCollectedNotification);
+                console.log('[notifications][collected][collected-notification][send]: All Notifications Sent');
+            }
+
+
+        } catch (err) {
+            console.log('[notifications][collected][collected-notification][send]:  Error', err);
+            return { code: 'UNKNOWN_ERROR', message: err.message, error: err };
+        }
+    },
+
+
+    sendInAppNotification: async (sender, receiver, data) => {
+
+        data = {
+            orgId: sender.orgId,
+            senderId: sender.id,
+            receiverId: receiver.id,
+            payload: {
+                ...data,
+                subject: 'Test Notification',
+                body: `Hi!!, Your parcel is collected`,
+                icon: 'assets/icons/icon-512x512.png',
+                image: 'assets/icons/icon-512x512.png',
+                extraData: {
+                    dateOfArrival: Date.now(),
+                    url: `/user/parcel`,
+                    primaryKey: Date.now()
+                }
+            },
+            actions: [
+                {
+                    action: "explore",
+                    title: "Open",
+                    url: `/user/parcel`
+                }
+            ]
+        }
+
+        return data;
+    },
+
+
+    sendEmailNotification: async (sender, receiver, data) => {
+        data = {
+            receiverEmail: receiver.email,
+            template: 'parcel-collected.ejs',
+            templateData: {
+                fullName: receiver.name
+            },
+            payload: {
+                ...data,
+                subject: 'Parcel Email Notification',
+            }
+        };
+
+        return data;
+    },
+
+    sendWebPushNotification: async (sender, receiver, data) => {
+
+        data = {
+            orgId: sender.orgId,
+            senderId: sender.id,
+            receiverId: receiver.id,
+            payload: {
+                subject: 'Test Notification',
+                body: `Hi!!, "Your parcel is Collected.`,
+                icon: 'assets/icons/icon-512x512.png',
+                image: 'assets/icons/icon-512x512.png',
+                extraData: {
+                    dateOfArrival: Date.now(),
+                    url: `${process.env.SITE_URL}/user/parcel`,
+                    primaryKey: Date.now()
+                }
+            },
+            actions: [
+                {
+                    action: "explore",
+                    title: "Open Home Page",
+                    url: `${process.env.SITE_URL}/user/parcel`
+                }
+            ]
+        }
+
+        return data;
+    },
+
+    sendSocketNotification: async (sender, receiver, data) => {
+        data = {
+
+        }
+        return data;
+    },
+
+
+    sendLineNotification: async (sender, receiver, data) => {
+        data = {
+            receiverId: receiver.id,
+            message: `Hi ${receiver.name} Your parcel is Collected.`
+        };
+
+        return data;
+    },
+
+    sendSMSNotification: async (sender, receiver, data) => {
+        data = {
+            receiverMobileNumber: receiver.mobileNo,
+            textMessage: `Hi ${receiver.name} this is simple text message send to test the notification`
+        }
+
+        return data;
+    }
+
+};
+
+module.exports = parcelCollectedNotification;
