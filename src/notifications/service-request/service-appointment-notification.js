7ed82e16639cb84a62c5d4ae675cf17e754320a5
System Administrator <root@Apple-2s-Mac-mini.local>
2021-01-06 15:34:17 +0530

    updated notification content

--- src/notifications/service-request/service-appointment-notification.js
+++ src/notifications/service-request/service-appointment-notification.js
@@ -49,7 +49,7 @@ const serviceAppointmentNotification = {
             payload: {
                 ...data,
                 subject: title,
-                body: description + `from  ${sender.name}`,
+                body: description,
                 icon: icons,
                 image: images,
                 extraData: {
@@ -133,7 +133,7 @@ const serviceAppointmentNotification = {
             receiverId: receiver.id,
             payload: {
                 subject: title,
-                body: description + `from ${sender.name}`,
+                body: description,
                 icon: icons,
                 image: images,
                 extraData: {
