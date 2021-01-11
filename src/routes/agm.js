a66902ce77342244bfed4bbbd47628be32216bea
Amar Pandey <amar@infiniseasoft.com>
2021-01-11 17:52:05 +0530

    AGM module api changes

--- src/routes/agm.js
+++ src/routes/agm.js
@@ -4,21 +4,8 @@ const router = express.Router();
 const authMiddleware = require("../middlewares/auth");
 const agmController = require("../controllers/agm");
 
-router.post(
-  "/save-announcement-notification",
-  authMiddleware.isAuthenticated,
-  agmController.saveAnnouncementNotifications
-);
+router.get("/generate-agm-id",authMiddleware.isAuthenticated,agmController.generateAGMId)
+router.post("/save-agm",authMiddleware.isAuthenticated,agmController.addAGMPreparation)
 
-router.get('/generate-announcement-id', authMiddleware.isAuthenticated, agmController.generateAnnouncementId);
 
-router.post('/save-announcement-as-draft', authMiddleware.isAuthenticated, agmController.saveAnnouncementAsDraft)
-
-router.post('/get-announcement-list', authMiddleware.isAuthenticated, agmController.getAnnouncementList)
-
-router.post('/get-announcement-details', authMiddleware.isAuthenticated, agmController.getAnnouncementDeatails)
-
-router.post('/delete-announcement', authMiddleware.isAuthenticated, agmController.deleteAnnouncementById)
-
-router.post('/resend-announcement-notification', authMiddleware.isAuthenticated, agmController.resendAnnouncementNotification)
 module.exports = router;
