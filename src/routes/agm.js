5b25518fe0654926cd41cf1b8e8ce92ec3018684
Alcanzar-Desk-05 <avdhesh.kumar@alcanzarsoft.com>
2021-01-15 19:04:58 +0530

    add api in agm module

--- src/routes/agm.js
+++ src/routes/agm.js
@@ -7,4 +7,8 @@ const agmController = require("../controllers/agm");
 router.get("/generate-agm-id", authMiddleware.isAuthenticated, agmController.generateAGMId)
 router.post("/save-agm", authMiddleware.isAuthenticated, agmController.addAGMPreparation)
 router.post("/import-owner-data", authMiddleware.isAuthenticated, agmController.importOwnerData);
+router.post("/add-owner", authMiddleware.isAuthenticated, agmController.addOwner);
+router.post("/delete-owner", authMiddleware.isAuthenticated, agmController.deleteOwner);
+router.post("/update-eligibility", authMiddleware.isAuthenticated, agmController.updateEligibility);
+
 module.exports = router;
