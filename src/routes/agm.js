356f5cd738c373a23d6ec7a7fef960817ad902ca
System Administrator <root@Apple-2s-Mac-mini.local>
2021-01-20 10:32:49 +0530

    updated agm apis

--- src/routes/agm.js
+++ src/routes/agm.js
@@ -5,7 +5,7 @@ const authMiddleware = require("../middlewares/auth");
 const agmController = require("../controllers/agm");
 
 router.get("/generate-agm-id", authMiddleware.isAuthenticated, agmController.generateAGMId)
-router.post("/save-agm", authMiddleware.isAuthenticated, agmController.addAGMPreparation)
+router.post("/save-agm", authMiddleware.isAuthenticated, agmController.addAgmPreparation)
 router.post("/import-owner-data", authMiddleware.isAuthenticated, agmController.importOwnerData);
 router.post("/add-owner", authMiddleware.isAuthenticated, agmController.addOwner);
 router.post("/delete-owner", authMiddleware.isAuthenticated, agmController.deleteOwner);
