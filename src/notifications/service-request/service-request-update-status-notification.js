44399742c20513ec8c2e5df7cce1da745c1336fc
Amar Pandey <amar@infiniseasoft.com>
2021-01-22 16:03:48 +0530

    Notification changes

--- src/notifications/service-request/service-request-update-status-notification.js
+++ src/notifications/service-request/service-request-update-status-notification.js
@@ -38,17 +38,6 @@ const serviceRequestUpdateStatusNotification = {
             icons = orgData.organisationLogo;
             images = orgData.organisationLogo;
         }
-        // if (orgData && orgData.id == '56') {
-        //     icons = 'assets/icons/cbre-512x512.png';
-        //     images = 'assets/icons/cbre-512x512.png';
-        // } else if (orgData && orgData.id == '89') {
-        //     icons = 'assets/icons/senses-512x512.png';
-        //     images = 'assets/icons/senses-512x512.png';
-        // }
-        // else {
-        //     icons = 'assets/icons/icon-512x512.png';
-        //     images = 'assets/icons/icon-512x512.png';
-        // }
 
         data = {
             orgId: sender.orgId,
@@ -118,30 +107,6 @@ const serviceRequestUpdateStatusNotification = {
             icons = orgData.organisationLogo;
             images = orgData.organisationLogo;
         }
-        // if (orgData) {
-        //     console.log("org data for notification", orgData)
-        //     if (orgData.id == '56') {
-        //         icons = 'assets/icons/cbre-512x512.png';
-        //         images = 'assets/icons/cbre-512x512.png';
-        //     } else {
-        //         icons = 'assets/icons/senses-512x512.png';
-        //         images = 'assets/icons/senses-512x512.png';
-        //     }
-        // } else {
-        //     icons = 'assets/icons/icon-512x512.png';
-        //     images = 'assets/icons/icon-512x512.png';
-        // }
-        // if(orgData && orgData.id == '56'){
-        //     icons = 'assets/icons/cbre-512x512.png';
-        //     images = 'assets/icons/cbre-512x512.png';
-        // }else if(orgData && orgData.id == '89'){
-        //     icons = 'assets/icons/senses-512x512.png';
-        //     images = 'assets/icons/senses-512x512.png';
-        // }
-        // else{
-        //     icons = 'assets/icons/icon-512x512.png';
-        //     images = 'assets/icons/icon-512x512.png';
-        // }
 
         data = {
             orgId: sender.orgId,
@@ -185,18 +150,6 @@ const serviceRequestUpdateStatusNotification = {
             icons = orgData.organisationLogo;
             images = orgData.organisationLogo;
         }
-        // if (orgData && orgData.id == '56') {
-        //     icons = 'assets/icons/cbre-512x512.png';
-        //     images = 'assets/icons/cbre-512x512.png';
-        // } else if (orgData && orgData.id == '89') {
-        //     icons = 'assets/icons/senses-512x512.png';
-        //     images = 'assets/icons/senses-512x512.png';
-        // }
-        // else {
-        //     icons = 'assets/icons/icon-512x512.png';
-        //     images = 'assets/icons/icon-512x512.png';
-        // }
-
         data = {
             orgId: sender.orgId,
             senderId: sender.id,
