af2644211671f1f1dd11957ac0eb707bccf6c0ac
Deepak Pandey <deepakpandeygonda@gmail.com>
2021-02-18 19:44:07 +0530

    Crons for workOrderOverdue and daily-digest

---
+++ src/helpers/daily-digest.js
@@ -0,0 +1,24 @@
+const Joi = require("@hapi/joi");
+const _ = require("lodash");
+const knex = require("../db/knex");
+
+
+const dailyDigestHelper = {
+  
+  prepareDailyDigestForUsers: async () => {
+    try {
+
+      const moment = require("moment-timezone");
+      let timezone = 'Asia/Bangkok';
+      moment.tz.setDefault(timezone);
+      let currentTime = moment().valueOf();
+      console.log('[helpers][daily-digest][prepareDailyDigestForUsers]:  Current Time:', moment().format("YYYY-MM-DD hh:mm A"));
+
+
+    } catch (err) {
+      console.error('[helpers][daily-digest][prepareDailyDigestForUsers]:  Error', err);
+      return { code: 'UNKNOWN_ERROR', message: err.message, error: err };
+    }
+  }
+};
+module.exports = dailyDigestHelper;
