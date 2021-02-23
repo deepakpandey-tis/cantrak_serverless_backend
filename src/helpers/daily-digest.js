1d334d2a6ac9acccfb2995e368e0168c7f71a035
Deepak Pandey <deepakpandeygonda@gmail.com>
2021-02-23 15:48:56 +0530

    Admin daily digest complete

--- src/helpers/daily-digest.js
+++ src/helpers/daily-digest.js
@@ -8,6 +8,46 @@ const emailHelper = require("../helpers/email");
 const timezone = 'Asia/Bangkok';
 moment.tz.setDefault(timezone);
 
+const getCardsData = async (orgId) => {
+  const [openRequests, openOrders, openSurveys, overDueWOrders] = await Promise.all([
+    knex
+      .from("service_requests")
+      .distinct("service_requests.id")
+      .where({ moderationStatus: true, orgId })
+      .whereIn("serviceStatusCode", ["US", "O"]),
+
+    knex
+      .from("service_requests")
+      .distinct("service_requests.id")
+      .where({ orgId: orgId })
+      .whereIn("serviceStatusCode", ["A", "IP", "OH"]),
+
+    knex
+      .from("survey_orders")
+      .distinct("survey_orders.id")
+      .where({
+        orgId: orgId,
+        surveyOrderStatus: 'Pending',
+      }),
+
+    knex
+      .from("task_group_schedule_assign_assets")
+      .distinct("task_group_schedule_assign_assets.id")
+      .where({
+        orgId: orgId,
+        status: 'O',
+        isOverdue: true
+      })
+  ]);
+
+  return {
+    openServiceRequestCount: openRequests ? openRequests.length : 0,
+    openServiceOrdersCount: openOrders ? openOrders.length : 0,
+    openSurveysCount: openSurveys ? openSurveys.length : 0,
+    overDueWorkOrdersCount: overDueWOrders ? overDueWOrders.length : 0,
+  };
+}
+
 const sendDailyDigestToOrgAdmins = async () => {
   const usersQuery = await knex.raw(`select * from users u join application_user_roles ur on ur."userId" = u.id 
    where u."isActive" = true and u."lastLogin" > (extract(epoch from (now() - interval '7 days')) * 1000)
@@ -17,6 +57,9 @@ const sendDailyDigestToOrgAdmins = async () => {
   const Parallel = require('async-parallel');
   await Parallel.each(users, async (user) => {
     console.log('[helpers][daily-digest][sendDailyDigestToOrgAdmins]: For User:', user.email);
+
+    let cardData = await getCardsData(user.orgId);
+    console.log('[helpers][daily-digest][sendDailyDigestToOrgAdmins]: Card Data:', cardData);
 
     // Find parts of given organisation...
     const partsQuery = await knex.raw(`select distinct pm.id, sum(pl.quantity) as "totalQuantity", pm."minimumQuantity",  pm."partName", pm."partCode", pm."orgId" 
@@ -33,6 +76,7 @@ const sendDailyDigestToOrgAdmins = async () => {
         templateData: {
           fullName: user.name,
           orgId: user.orgId,
+          cardData: cardData,
           parts: parts
         },
         orgId: user.orgId
@@ -75,15 +119,15 @@ const dailyDigestHelper = {
       console.log('[helpers][daily-digest][prepareDailyDigestForUsers]: Processing Start - Current Time:', moment().format("YYYY-MM-DD hh:mm A"));
 
       try {
-        // await sendDailyDigestToOrgAdmins();
+        await sendDailyDigestToOrgAdmins();
       } catch (err) {
-        console.log('[helpers][daily-digest][prepareDailyDigestForUsers]: Some error occured in preparing daily digest for admins');
+        console.log('[helpers][daily-digest][prepareDailyDigestForUsers]: Some error occured in preparing daily digest for admins', err);
       }
 
       try {
         // await sendDailyDigestToOrgUsers();
       } catch (err) {
-        console.log('[helpers][daily-digest][prepareDailyDigestForUsers]: Some error occured in preparing daily digest for org users');
+        console.log('[helpers][daily-digest][prepareDailyDigestForUsers]: Some error occured in preparing daily digest for org users', err);
       }
 
       console.log('[helpers][daily-digest][prepareDailyDigestForUsers]: Processing End - Current Time:', moment().format("YYYY-MM-DD hh:mm A"))
