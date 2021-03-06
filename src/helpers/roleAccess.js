a2bfb6c96ef19a7d9a30dfeab90b02fa3cfdea24
amar460 <pandeypiyush460@gmail.com>
2021-03-06 16:16:01 +0530

    RoleAccess helper added

--- src/helpers/roleAccess.js
+++ src/helpers/roleAccess.js
@@ -11,18 +11,38 @@ AWS.config.update({
 });
 
 const roleAccessHelper = {
-    getAllUsers : async ({projectId,resourceId}) =>{
-        try {
-            
-            let users = await knex
-            .from("users")
-            .leftJoin("")
+  getAllUsers: async ({ projectId, resourceId,orgId }) => {
+    console.log("[getAllUsers][payload]",projectId,resourceId,orgId)
 
-        } catch (err) {
-            
-        }
+    try {
+      let users = await knex
+        .from("users")
+        .leftJoin("team_users", "users.id", "team_users.userId")
+        .leftJoin(
+          "team_roles_project_master",
+          "team_users.teamId",
+          "team_roles_project_master.teamId"
+        )
+        .leftJoin(
+          "role_resource_master",
+          "team_roles_project_master.roleId",
+          "role_resource_master.roleId"
+        )
+        .select(["users.*"])
+        .where({
+            "team_roles_project_master.projectId":projectId,
+            "role_resource_master.resourceId":resourceId,
+            "users.orgId":orgId
+        });
+
+        console.log("[helpers][roleAccess][getAllUsers]:  result", users);
+
+         return users;
+    } catch (err) {
+
+        console.log("[helpers][roleAccess][getAllUsers]:  Error", err);
+        return { code: "UNKNOWN_ERROR", message: err.message, error: err };
     }
-    
-}
-
-
+  },
+};
+module.exports = roleAccessHelper;
