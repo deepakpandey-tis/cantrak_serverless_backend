ac252515f33c2d94e0d4abc7c62113ea53be6e64
amar460 <pandeypiyush460@gmail.com>
2021-03-11 16:04:10 +0530

    PM report error resolved

--- src/routes/preventive-maintenance.js
+++ src/routes/preventive-maintenance.js
@@ -6,34 +6,125 @@ const roleMiddleware = require("../middlewares/role");
 const resourceAccessMiddleware = require("../middlewares/resourceAccessMiddleware");
 const pmController = require("../controllers/preventive-maintenance");
 
-router.post("/get-feedbacks-details", authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, resourceAccessMiddleware.isPMAccessible, pmController.viewFeedbacksReport);
-router.post('/get-pm-asset-list-by-filter', authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, resourceAccessMiddleware.isPMAccessible, pmController.getPmAssetListByFilter)
-router.post('/pm-post-feedback-for-asset', authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, resourceAccessMiddleware.isPMAccessible, pmController.postFeedbackWithImages)
-router.post('/get-pm-feedback-list', authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, resourceAccessMiddleware.isPMAccessible, pmController.getFeedbackList)
-router.post('/get-task-list-bypmid', authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, resourceAccessMiddleware.isPMAccessible, pmController.getTaskListByPmId)
+router.post(
+  "/get-feedbacks-details",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  pmController.viewFeedbacksReport
+);
+router.post(
+  "/get-pm-asset-list-by-filter",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  pmController.getPmAssetListByFilter
+);
+router.post(
+  "/pm-post-feedback-for-asset",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  pmController.postFeedbackWithImages
+);
+router.post(
+  "/get-pm-feedback-list",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  pmController.getFeedbackList
+);
+router.post(
+  "/get-task-list-bypmid",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  pmController.getTaskListByPmId
+);
 
-router.post('/get-asset-list-of-pm', authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, resourceAccessMiddleware.isPMAccessible, pmController.getAssetListOfPm)
-router.post('/get-asset-list-with-completed-pm', authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, resourceAccessMiddleware.isPMAccessible, pmController.getAssetListWithCompletedPm)
+router.post(
+  "/get-asset-list-of-pm",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  pmController.getAssetListOfPm
+);
+router.post(
+  "/get-asset-list-with-completed-pm",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  pmController.getAssetListWithCompletedPm
+);
 
-router.post('/get-asset-id-by-asset-serial-or-barcode', authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, resourceAccessMiddleware.isPMAccessible, pmController.getAssetIdByAssetSerialOrBarcode)
+router.post(
+  "/get-asset-id-by-asset-serial-or-barcode",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  pmController.getAssetIdByAssetSerialOrBarcode
+);
 
 // Update asset Pm Endate
-router.post('/update-asset-pm-endDate', authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, resourceAccessMiddleware.isPMAccessible, pmController.updateAssetPm)
+router.post(
+  "/update-asset-pm-endDate",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  pmController.updateAssetPm
+);
 
 // Get Pm Report
-router.post('/get-pm-report', authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, resourceAccessMiddleware.isPMAccessible, pmController.getPmReport)
-router.post('/save-pm-template', authMiddleware.isAuthenticated, authMiddleware.isAuthenticated, pmController.savePMTemplate)
-router.post('/search-pm-template', authMiddleware.isAuthenticated, authMiddleware.isAuthenticated, pmController.searchPMTemplate)
-router.post('/search-pm-by-name', authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, resourceAccessMiddleware.isPMAccessible, pmController.getPmByName)
+router.post(
+  "/get-pm-report",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  pmController.getPmReport
+);
+router.post(
+  "/save-pm-template",
+  authMiddleware.isAuthenticated,
+  authMiddleware.isAuthenticated,
+  pmController.savePMTemplate
+);
+router.post(
+  "/search-pm-template",
+  authMiddleware.isAuthenticated,
+  authMiddleware.isAuthenticated,
+  pmController.searchPMTemplate
+);
+router.post(
+  "/search-pm-by-name",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  pmController.getPmByName
+);
 
-router.post('/pm-report', authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, resourceAccessMiddleware.isPMAccessible, pmController.pmReport)
-router.post('/pm-history-report', authMiddleware.isAuthenticated, roleMiddleware.parseUserPermission, resourceAccessMiddleware.isPMAccessible, pmController.pmHistoryReport)
+router.post(
+  "/pm-report",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  pmController.pmReport
+);
+router.post(
+  "/pm-history-report",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  pmController.pmHistoryReport
+);
 
-router.post('/pm-plan-action-schedule-report',
-    authMiddleware.isAuthenticated,
-    authMiddleware.isOrgAdmin,
-    pmController.pmScheduleReport)
+router.post(
+  "/pm-plan-action-schedule-report",
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isPMAccessible,
+  pmController.pmScheduleReport
+);
 
-router.get('/pm-status-clone', pmController.pmStatusClone)
+router.get("/pm-status-clone", pmController.pmStatusClone);
 
-module.exports = router
+module.exports = router;
