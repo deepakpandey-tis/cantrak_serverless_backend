3692d25969b4b374c217b614aa9529604a7cf207
amar460 <pandeypiyush460@gmail.com>
2021-04-01 10:15:14 +0530

    WOrk order overdue changes

--- src/routes/test.js
+++ src/routes/test.js
@@ -194,6 +194,88 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/open" ,async (req,res) =>{
+  try {
+    
+
+    let currentTime = new Date().getTime();
+
+    let currentDate = moment().format("YYYY-MM-DD");
+
+    console.log("current date", currentDate);
+
+    let workOrders = await knex(
+      "task_group_schedule_assign_assets"
+    )
+      .select(['id', 'status', 'completedAt'])
+      .whereRaw(
+        `to_date(task_group_schedule_assign_assets."pmDate",'YYYY-MM-DD')<'${currentDate}'`
+      )
+      .where((qb)=>{
+          qb.where({ status: "O" })
+          // qb.orWhere({status: "COM"})
+      })
+      .whereNull('completedAt');
+
+    console.log("work orders", workOrders);
+
+    const Parallel = require("async-parallel");
+    Parallel.setConcurrency(20);
+
+    await Parallel.each(workOrders, async (pd) => {
+      let workResult = await knex("pm_task")
+        .count("*")
+        .where({
+          taskGroupScheduleAssignAssetId: pd.id,
+        })
+        .first();
+
+      let completedTask = await knex("pm_task")
+        .count("*")
+        .where({
+          taskGroupScheduleAssignAssetId: pd.id,
+          status: "COM",
+        })
+        .first();
+
+      console.log(
+        "work result====>>>",
+        workResult,
+        "=======",
+        completedTask
+      );
+
+      if (workResult.count == completedTask.count) {
+        let maxTime = await knex("pm_task")
+          .max("updatedAt")
+          .where({
+            taskGroupScheduleAssignAssetId: pd.id,
+            status: "COM",
+          })
+          .first();
+
+        console.log("max time====>>>", maxTime);
+
+        if (maxTime) {
+          await knex(
+            "task_group_schedule_assign_assets"
+          )
+            .update({
+              status: "COM",
+              updatedAt: currentTime,
+              completedAt: maxTime.max,
+            })
+            .where({ id: pd.id });
+        }
+      }
+    });
+
+  } catch (err) {
+    res.status(200).json({ failed: true, error: err });
+    
+  }
+});
+
 router.get("/print-registration", (req, res) => {
   console.log(req.query);
   const path = require("path");
