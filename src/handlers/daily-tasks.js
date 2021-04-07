3742698ad72afbf29bcd316a06006a45207271e5
Deepak Pandey <deepakpandeygonda@gmail.com>
2021-04-07 13:10:03 +0530

    Change Hndlers folder

--- src/handlers/daily-tasks.js
+++ src/handlers/daily-tasks.js
@@ -1,9 +1,37 @@
 const knex = require('./db/knex');
+const AWS = require('aws-sdk');
 
 module.exports.dBWakeUpTask = async (event, context) => {
-  
+  console.log('[handlers][dBWakeUpTask]: Event:', JSON.stringify(event));
+  // console.log('[handlers][dBWakeUpTask]: Context:', JSON.stringify(context));
+
   await knex('users').limit(1);
   console.log('[handlers][dBWakeUpTask]: Task Completed Successfully');
 
+  return true;
+};
+
+
+module.exports.workOrderOverdueProcessor = async (event, context) => {
+  // console.log('[handlers][workOrderOverdueProcessor]: Event:', JSON.stringify(event));
+  // console.log('[handlers][workOrderOverdueProcessor]: Context:', JSON.stringify(context));
+
+  const pmHelper = require("../helpers/preventive-maintenance");
+  await pmHelper.markWorkOrdersOverDue();
+
+  console.log('[handlers][workOrderOverdueProcessor]: Task Completed Successfully');
+
+  return true;
+};
+
+
+module.exports.dailyDigestProcessor = async (event, context) => {
+  console.log('[handlers][dailyDigestProcessor]: Event:', JSON.stringify(event));
+  // console.log('[handlers][workOrderOverdueProcessor]: Context:', JSON.stringify(context));
+
+  const dailyDigestHelper = require("../helpers/daily-digest");
+  await dailyDigestHelper.prepareDailyDigestForUsers();
+
+  console.log('[handlers][dailyDigestProcessor]: Task Completed Successfully');
   return true;
 };
---
+++ src/handlers/jobs.js
@@ -0,0 +1,236 @@
+const knex = require('./db/knex');
+const AWS = require('aws-sdk');
+
+const readJsonFile = async (Bucket, Key) => {
+
+  AWS.config.update({
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    region: process.env.REGION || "us-east-1"
+  });
+
+  const params = {
+    Bucket,
+    Key,
+    ResponseContentType: 'application/json',
+  };
+
+  const s3 = new AWS.S3({
+    'signatureVersion': 'v4'
+  });
+
+  const f = await s3.getObject(params).promise();
+  return f.Body.toString('utf-8');
+};
+
+
+
+// EMAIL HANDLER (Triggered From SQS)
+module.exports.queueProcessor = async (event, context) => {
+  // console.log('Event:', JSON.stringify(event));
+  // console.log('Context:', JSON.stringify(context));
+
+  const recordsFromSQS = event.Records;
+  const currentRecord = recordsFromSQS[0];    // Since we have kept the batchSize to only 1
+  console.log('Current Record:', currentRecord);
+
+  let recordData = JSON.parse(currentRecord.body);
+  console.log('[longJobsProcessor] recordData:', recordData);
+
+  if (currentRecord && recordData.payloadType && recordData.payloadType == 's3') {
+    console.log('[longJobsProcessor] Got S3 Link, Sqs Message (as json file):', recordData.s3FileKey);
+    let jsonData = await readJsonFile(process.env.S3_BUCKET_NAME, recordData.s3FileKey);
+    console.log('[longJobsProcessor] Json Data from s3:', jsonData);
+    recordData = JSON.parse(jsonData);
+  }
+
+
+  let messageType = 'EMAIL';
+
+  if (currentRecord.messageAttributes && currentRecord.messageAttributes.messageType) {
+    messageType = currentRecord.messageAttributes.messageType.stringValue;
+  }
+  console.log('[handlers][queueProcessor]', 'Message Type:', messageType);
+
+
+  if (messageType === 'EMAIL') {
+
+    const emailHelper = require('../helpers/email');
+    const mailOptions = recordData;
+    await emailHelper.sendEmail(mailOptions);
+
+    console.log('[handlers][queueProcessor]: Email Sent Successfully');
+
+  }
+
+  if (messageType === 'NOTIFICATION') {
+    console.log('[handlers][queueProcessor]', 'Received message is notification.');
+    const notificationHandler = require('../notifications/core/notification');
+    const notificationOptions = recordData;
+
+    console.log('[handlers][queueProcessor]: Notification Options:', notificationOptions);
+    await notificationHandler.processQueue(notificationOptions);
+
+    console.log('[handlers][queueProcessor]: Notification Sent Successfully');
+  }
+
+  return true;
+};
+
+
+module.exports.longJobsProcessor = async (event, context) => {
+  // console.log('Event:', JSON.stringify(event));
+  // console.log('Context:', JSON.stringify(context));
+
+  const recordsFromSQS = event.Records;
+  const currentRecord = recordsFromSQS[0];    // Since we have kept the batchSize to only 1
+  console.log('[handlers][longJobsProcessor] Current Record:', currentRecord);
+
+  let recordData = JSON.parse(currentRecord.body);
+  console.log('[handlers][longJobsProcessor] recordData:', recordData);
+
+
+  if (currentRecord && recordData.payloadType && recordData.payloadType == 's3') {
+    console.log('[handlers][longJobsProcessor] Got S3 Link, Sqs Message (as json file):', recordData.s3FileKey);
+    let jsonData = await readJsonFile(process.env.S3_BUCKET_NAME, recordData.s3FileKey);
+    console.log('[handlers][longJobsProcessor] Json Data from s3:', jsonData);
+    recordData = JSON.parse(jsonData);
+  }
+
+  let messageType = '';
+
+  if (currentRecord.messageAttributes && currentRecord.messageAttributes.messageType) {
+    messageType = currentRecord.messageAttributes.messageType.stringValue;
+  }
+  console.log('[handlers][longJobsProcessor]', 'Message Type:', messageType);
+
+
+  if (messageType === 'PM_WORK_ORDER_GENERATE') {
+
+    const creatPmHelper = require("../helpers/preventive-maintenance");
+    const { consolidatedWorkOrders, payload, orgId, requestedBy, orgMaster } = recordData;
+
+    if (consolidatedWorkOrders && payload) {
+      console.log('work orders ==============>>>>>>>>>>', consolidatedWorkOrders, orgMaster)
+      pmWorkOrder = await creatPmHelper.createWorkOrders({ consolidatedWorkOrders, payload, orgId });
+      console.log("pmWorkOrder result ======>>>>>", pmWorkOrder);
+
+      if (pmWorkOrder) {
+        const createPmLongJobsNotification = require("../notifications/preventive-maintenance/long-jobs-notification");
+
+        const ALLOWED_CHANNELS = ['IN_APP', 'WEB_PUSH', 'SOCKET_NOTIFY']
+
+        let dataNos = {
+          payload: {
+            orgData: orgMaster
+          },
+        };
+
+
+        let receiver = requestedBy
+        let sender = requestedBy
+
+        await createPmLongJobsNotification.send(
+          sender,
+          receiver,
+          dataNos,
+          ALLOWED_CHANNELS
+        )
+      }
+    } else {
+      console.log('[handlers][longJobsProcessor]', 'Work Orders Cannot be generated. Wrong Data in Payload');
+      throw Error('Work Orders Cannot be generated. Wrong Data in Payload');
+    }
+
+    console.log('[handlers][longJobsProcessor]: Task Completed...');
+
+  }
+
+  if (messageType === 'ANNOUNCEMENT_BROADCAST') {
+
+    console.log('[handlers][longJobsProcessor]', 'Data For Announcement:', recordData);
+
+    const announcementHelper = require('../helpers/announcement');
+
+    const { announcementId, dataNos, ALLOWED_CHANNELS, orgId, requestedBy, orgMaster } = recordData;
+
+    if (announcementId) {
+      await announcementHelper.sendAnnouncement({ announcementId, dataNos, ALLOWED_CHANNELS, orgId, requestedBy });
+    } else {
+      console.log('[handlers][longJobsProcessor]', 'Announcement Id not found. Hence Announcement can not be broadcasted.');
+      throw Error('Announcement Id not found. Hence Announcement can not be broadcasted.');
+    }
+
+    console.log('[handlers][longJobsProcessor]: Task Completed.....');
+
+  }
+
+
+  if (messageType == 'AGM_PREPARE_VOTING_DOCUMENT') {
+
+    console.log('[handlers][longJobsProcessor]: Data For AGM Voting Doc Prepare:', recordData);
+
+    const agmHelper = require('../helpers/agm');
+
+    const { agmId, data, orgId, requestedBy } = recordData;
+
+    if (agmId) {
+      // await agmHelper.generateVotingDocument({ agmId, data, orgId, requestedBy });
+      await agmHelper.generateVotingDocumentOnEFS({ agmId, data, orgId, requestedBy });
+    } else {
+      console.log('[handlers][longJobsProcessor]', 'AGM Id not found. Hence Voting Documents can not be generated.');
+      throw Error('AGM Id not found. Hence Voting Documents can not be generated.');
+    }
+
+    console.log('[handlers][longJobsProcessor]: Task Completed.....');
+
+  }
+
+
+
+  if (messageType == 'AGM_FINAL_SUBMIT') {
+
+    console.log('[handlers][longJobsProcessor]: Data For AGM_FINAL_SUBMIT:', recordData);
+
+    const agmHelper = require('../helpers/agm');
+
+    const { agmId, data, orgId, requestedBy } = recordData;
+
+    if (agmId) {
+      await agmHelper.finalSubmit({ agmId, data, orgId, requestedBy });
+    } else {
+      console.log('[handlers][longJobsProcessor]', 'AGM Id not found. Hence AGM_FINAL_SUBMIT can not be done.');
+      throw Error('AGM Id not found. AGM Id not found. Hence AGM_FINAL_SUBMIT can not be done.');
+    }
+
+    console.log('[handlers][longJobsProcessor]: Task Completed.....');
+
+  }
+
+  if (messageType == 'TEST_PROCESSOR') {
+
+    console.log('[handlers][longJobsProcessor]: Data For TEST_PROCESSOR:', recordData);
+
+    const testHelper = require('../helpers/test');
+
+    const { type } = recordData;
+    console.log("[handlers][longJobsProcessor]: TEST_PROCESSOR: Messgae TYPE:", type)
+
+    if (type == 'COMPLETED') {
+      // await testHelper.setCompletedWO();
+    } else if (type == 'OPEN') {
+      // await testHelper.setOpenWorkOrder();
+    } else if (type == 'efs') {
+      await testHelper.testEFS();
+    } else {
+      console.log('[handlers][longJobsProcessor]', 'Type not found. Hence TEST_PROCESSOR can not be run.');
+      throw Error('Type not found. Type not found. Hence TEST_PROCESSOR can not be run.');
+    }
+
+    console.log('[handlers][longJobsProcessor]: Task Completed.....');
+
+  }
+
+  console.log('[handlers][longJobsProcessor]: Finished.....');
+  return true;
+};
