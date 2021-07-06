a3cc780ae00a9b8bdd588569b764fa188dcb0e56
Atif Ahmad <atif.ahmad@alcanzarsoft.com>
2021-07-06 17:02:11 +0530

    2 image rekognition api has been done

--- src/controllers/visitor/get-self-registration-property-units.js
+++ src/controllers/visitor/get-self-registration-property-units.js
@@ -14,7 +14,7 @@ const getSelfRegistrationPropertyUnits = async (req, res) => {
         , pu."projectId", p."projectName", pu."buildingPhaseId", bap."buildingPhaseCode"
         , bap.description as "buildingDescription",pu."floorZoneId", faz."floorZoneCode", faz.description
         , pu."unitNumber", pu.id as "unitId"
-        , u.id as "tenantId", u."name" as "tenantName"`;
+        , u.id as "tenantId", u."name" as "tenantName", uha."id" as "userHouseAllocationId"`;
 
         sqlFrom = ` FROM companies c, projects p, buildings_and_phases bap, floor_and_zones faz, property_units pu, user_house_allocation uha , users u`;
 
