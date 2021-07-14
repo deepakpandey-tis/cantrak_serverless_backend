dd7454d4a54b6eaaeed0ef0991dc277bc58c7c19
Deepak Pandey <deepakpandeygonda@gmail.com>
2021-07-15 00:23:36 +0530

    SCB Bill Payment Modifications as per krishna

--- src/controllers/bill-payment/scb_payment_bill_enquiry.js
+++ src/controllers/bill-payment/scb_payment_bill_enquiry.js
@@ -1,50 +1,6 @@
 const knex = require('../../db/knex');
-const axios = require('axios');
 const execDbProcedure = require('../../helpers/db/exec-db-procedure');
-const scbPGConstants = require('./constants');
-
-
-
-const scbPaymentGeToken = async () => {
-    const data = JSON.stringify({
-        "applicationKey": scbPGConstants.scbApplicationKey,
-        "applicationSecret": scbPGConstants.scbApplicationSecret,
-        "authCode": scbPGConstants.scbAuthCode
-    });
-
-    const config = {
-        method: 'post',
-        url: get_token_url,
-        headers: {
-            "Content-Type": "application/json",
-            "resourceOwnerId": scbPGConstants.scbApplicationKey,
-            "requestUId": scbPGConstants.scbRequestUId,
-            "accept-language": scbPGConstants.scbAcceptLanguage,
-            "Cookie": scbPGConstants.scbCookie
-        },
-        data: data
-    };
-
-    return axios(config);
-};
-
-const scbpaymentBillInquiryAPI = async (accessToken, transactionDate) => {
-    let data = '';
-    const config = {
-        method: 'get',
-        url: scbPGConstants.billInquiry_url + scbPGConstants.scbppId + '&reference1=' + scbPGConstants.reference1 + '&reference2=' + scbPGConstants.reference2 + '&transactionDate=' + transactionDate,
-        headers: {
-            'Content-Type': "application/json",
-            'authorization': 'Bearer ' + accessToken,
-            'requestUID': scbPGConstants.scbRequestUId,
-            'resourceOwnerID': scbPGConstants.scbApplicationKey,
-            'accept-language': scbPGConstants.scbAcceptLanguage,
-            'Cookie': scbPGConstants.scbCookie
-        },
-        data: data
-    };
-    return axios(config);
-}
+const billPaymentHelper = require('../../helpers/bill-payment');
 
 
 const scbPaymentBillEnquiry = async (req, res) => {
@@ -59,100 +15,103 @@ const scbPaymentBillEnquiry = async (req, res) => {
         }
         let dbret = await execDbProcedure(queryData);
 
+        let scbPGDynamicConstants = dbret.return_value[0].payment_api_json // changes
+        // scbApplicationKey = scbPGDynamicConstants.scbApplicationKey;
+        // scbApplicationSecret = scbPGDynamicConstants.scbApplicationSecret;
+        // get_token_url = scbPGDynamicConstants.get_token_url;
+        createQR_url = scbPGDynamicConstants.createQR_url;
+        billInquiry_url = scbPGDynamicConstants.billInquiry_url;
+        transactionPull_url = scbPGDynamicConstants.transactionPull_url;
+        scbppType = scbPGDynamicConstants.scbppType;
+        scbppId = scbPGDynamicConstants.scbppId;
+        sendingBank = scbPGDynamicConstants.sendingBank;
+        scb_reference3 = scbPGDynamicConstants.scb_reference3;
+
+
         let transactionDate = dbret.return_value[0].transaction_date_str;
 
         //in case a different date is sent from front end for checking
         if (check_transaction_date) {
             transactionDate = check_transaction_date;
         }
-        reference1 = dbret.return_value[0].payment_log_ref_id;
-        reference2 = dbret.return_value[0].payment_log_ref_id;
-        PaymentAmount = dbret.return_value[0].actual_pay_amount;
+        let referenceId = dbret.return_value[0].payment_log_ref_id;
+        let reference2 = dbret.return_value[0].payment_log_ref_id;
+        let PaymentAmount = dbret.return_value[0].actual_pay_amount;
 
         let reqstr = {};
         let resstr = {};
+        let accessToken;
+
+
         //start token block, to fetch token and save to database
-        {
-            let scbtokendata = await scbPaymentGeToken();
-            //console.log(scbtokendata);
-            if (scbtokendata.data.status.code == "1000") {
-                accessToken = scbtokendata.data.data.accessToken;
-            };
+        let scbtokendata = await billPaymentHelper.scbPaymentGeToken(scbPGDynamicConstants);
+        //console.log(scbtokendata);
+        if (scbtokendata.data.status.code == "1000") {
+            accessToken = scbtokendata.data.data.accessToken;
+        };
 
-            if (accessToken != 'test') {
-                let qrystr = { "query_name": "payment_log_json_req_res_save" };
-                qrystr.payment_log_rid = pre_record_id;
-                qrystr.record_id = null;
-                qrystr.request_description = 'Request for SCB Token';
-                qrystr.actual_pay_amount = PaymentAmount; //replace with actual later
+        if (accessToken) {
+            let qrystr = { "query_name": "payment_log_json_req_res_save" };
+            qrystr.payment_log_rid = pre_record_id;
+            qrystr.record_id = null;
+            qrystr.request_description = 'Request for SCB Token';
+            qrystr.actual_pay_amount = PaymentAmount; //replace with actual later
 
-                //grab the request json
-                reqstr.status = scbtokendata.status;
-                reqstr.statusText = scbtokendata.statusText;
-                reqstr.headers = scbtokendata.headers;
-                reqstr.config = scbtokendata.config;
-                qrystr.request_json = reqstr;
-                // console.log(reqstr);
+            //grab the request json
+            reqstr.status = scbtokendata.status;
+            reqstr.statusText = scbtokendata.statusText;
+            reqstr.headers = scbtokendata.headers;
+            reqstr.config = scbtokendata.config;
+            qrystr.request_json = reqstr;
+            // console.log(reqstr);
 
-                //grab the response json
-                resstr.data = scbtokendata.data;
-                qrystr.response_json = resstr;
+            //grab the response json
+            resstr.data = scbtokendata.data;
+            qrystr.response_json = resstr;
 
-                await execDbProcedure(qrystr);
-            }
-        }      //end token block, to fetch token and save to database
+            await execDbProcedure(qrystr);
+        }
+        //end token block, to fetch token and save to database
+
 
         //start of block to do bill inquiry
         let payment_success_flag = false;
         reqstr = {};
         resstr = {};
 
-        //console.log(accessToken);
-        //return null;
-        if (accessToken != 'test') {
-            let scbBillInquiryRet;
+       
+        if (accessToken) {
             try {
-                scbBillInquiryRet = await scbpaymentBillInquiryAPI(accessToken, transactionDate);
-                if (scbBillInquiryRet.status == "200" && scbBillInquiryRet.data.status.code == "1000" && scbBillInquiryRet.data.data[0].billPaymentRef1 == reference1) {
+                let scbBillInquiryRet = await billPaymentHelper.scbpaymentBillInquiryAPI(accessToken, transactionDate, referenceId, scbPGDynamicConstants);
+                if (scbBillInquiryRet.data.status.code == "1000" && scbBillInquiryRet.data.data[0].billPaymentRef1 == referenceId) {
                     //console.log("here");
                     payment_success_flag = true;
-                    reqstr.status = scbBillInquiryRet.status;
-                    reqstr.statusText = scbBillInquiryRet.statusText;
-                    reqstr.headers = scbBillInquiryRet.headers;
-                    reqstr.config = scbBillInquiryRet.config;
-
-                    resstr.data = scbBillInquiryRet.data;
-                    //console.log(scbBillInquiryRet.data);
+                    reqstr = {
+                        ...scbBillInquiryRet,
+                    };
                     //console.log(reqstr);
                 }
-
             } catch (error) {
                 //console.log(error.response);
-                //resstr ={};
-                reqstr.status = error.response.status;
-                reqstr.statusText = error.response.statusText;
-                reqstr.headers = error.response.headers;
-                reqstr.config = error.response.config;
-
-                resstr.data = error.response.data;
+                reqstr = {
+                    ...error.response,
+                };
             }
-            // console.log(scbBillInquiryRet.data.data);
 
             //start fetch bill inquiry block, save to database
-            {
-                queryData = {
-                    query_name: "payment_log_json_req_res_save",
-                    "payment_success_flag": payment_success_flag,
-                    payment_log_rid: pre_record_id,
-                    record_id: null,
-                    request_description: 'Bill Payment Inquiry',
-                    actual_pay_amount: PaymentAmount,
-                    request_json: reqstr,
-                    response_json: resstr,
-                };
+            queryData = {
+                query_name: "payment_log_json_req_res_save",
+                "payment_success_flag": payment_success_flag,
+                payment_log_rid: pre_record_id,
+                record_id: null,
+                request_description: 'Bill Payment Inquiry',
+                actual_pay_amount: PaymentAmount,
+                request_json: reqstr,
+                response_json: resstr,
+            };
 
-                await execDbProcedure(qrystr);
-            }      //end fetch bill inquiry block, save to database     
+            await execDbProcedure(qrystr);
+            //end fetch bill inquiry block, save to database     
         }
         res.status(200).send(dbret);
 
---
+++ src/helpers/bill-payment.js
@@ -0,0 +1,88 @@
+const _ = require("lodash");
+const AWS = require("aws-sdk");
+const knex = require("../db/knex");
+const axios = require("axios");
+
+const scbRequestUId = 'b341d8ef-0d19-4156-aef7-9c2542bad9f2'; //user device Unique ID
+const scbAcceptLanguage = 'EN'; // based on frontend selection EN/TH
+const scbCookie = 'TS01e7ba6b=01e76b033c9b0793ae51650c67e5d1645c08795b5f3a8f814509ab2916a9e1c0b638e1b73c769866f7c81592242f1bd0dda4c6d043';
+const scbAuthCode = "";
+
+
+const billPaymentHelper = {
+
+  scbPaymentGeToken: async ({ scbApplicationKey, scbApplicationSecret, get_token_url }) => {
+
+    const data = JSON.stringify({
+      "applicationKey": scbApplicationKey,
+      "applicationSecret": scbApplicationSecret,
+      "authCode": scbAuthCode
+    });
+
+    const config = {
+      method: 'post',
+      url: get_token_url,
+      headers: {
+        "Content-Type": "application/json",
+        "resourceOwnerId": scbApplicationKey,
+        "requestUId": scbRequestUId,
+        "accept-language": scbAcceptLanguage,
+        "Cookie": scbCookie
+      },
+      data: data
+    };
+
+    return axios(config);
+  },
+
+
+  scbpaymentCreateThaiQRCode: async (accessToken, PaymentAmount, referenceId, { scbApplicationKey, scbppType, scbppId, createQR_url }) => {
+    const data = JSON.stringify({
+      "qrType": "PP",
+      "ppType": scbppType,
+      "ppId": scbppId,
+      "amount": PaymentAmount,
+      "ref1": referenceId,
+      "ref2": referenceId,
+      "ref3": "CIE" //"ref3": "XCT"
+    });
+    //console.log(accessToken);
+    const config = {
+      method: 'post',
+      url: createQR_url,
+      headers: {
+        "Content-Type": "application/json",
+        "authorization": "Bearer " + accessToken,
+        "resourceOwnerId": scbApplicationKey,
+        "requestUId": scbRequestUId,
+        "accept-language": scbAcceptLanguage,
+        "Cookie": scbCookie
+      },
+      data: data
+    };
+    //console.log(config);
+    return axios(config)
+  },
+
+
+  scbpaymentBillInquiryAPI: async (accessToken, transactionDate, referenceId, {scbApplicationKey, billInquiry_url, scbppId }) => {
+    let data = '';
+    const config = {
+      method: 'get',
+      url: billInquiry_url + scbppId + '&reference1=' + referenceId + '&reference2=' + referenceId + '&transactionDate=' + transactionDate,
+      headers: {
+        'Content-Type': "application/json",
+        'authorization': 'Bearer ' + accessToken,
+        'resourceOwnerID': scbApplicationKey,
+        "requestUId": scbRequestUId,
+        "accept-language": scbAcceptLanguage,
+        "Cookie": scbCookie
+      },
+      data: data
+    };
+    return axios(config);
+  }
+
+};
+
+module.exports = billPaymentHelper;
