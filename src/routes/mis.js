14b7059d7236ac50c92995bcdf5ba2b03f6e933e
Vineet Bhasin <Vineet.Bhasin@grafixsoftware.com>
2021-11-29 22:10:07 +0530

    product card (product lotno detail)

---
+++ src/routes/mis.js
@@ -0,0 +1,17 @@
+const { Router } = require("express")
+const path = require("path")
+const router = Router()
+const authMiddleware = require('../middlewares/auth')
+const roleMiddleware = require('../middlewares/role')
+const resourceAccessMiddleware = require('../middlewares/resourceAccessMiddleware');
+const misController = require('../controllers/mis');
+
+
+router.post('/get-product-lotno-detail',
+  authMiddleware.isAuthenticated,
+  roleMiddleware.parseUserPermission,
+  resourceAccessMiddleware.isAssetAccessible,
+  misController.getProductLotNoDetail
+);
+
+module.exports = router;
