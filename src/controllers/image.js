c4aa10d51ae165d88165b50ac390fe3e94ec8d6b
Atif Ahmad <atif.ahmad@alcanzarsoft.com>
2022-03-06 15:20:27 +0530

    add api for upload image tags

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -37,7 +37,7 @@ const imageController = {
         }
       })
     } catch (err) {
-      console.log('[controllers][quotation][updateQuotation] :  Error', err);
+      console.log('[controllers][image][deleteImage] :  Error', err);
       res.status(500).json({
         errors: [
           { code: 'UNKNOWN_SERVER_ERROR', message: err.message }
@@ -45,16 +45,55 @@ const imageController = {
       });
     }
   },
+
   uploadImageByEntity:async (req,res) => {
     try {
+      const currentTime = new Date().getTime();
       const payload = req.body;
-      const uploadedImage = await knex('images').insert(payload).returning(['*']);
+      const uploadedImage = await knex('images').insert({ ...payload, orgId: req?.me?.orgId, createdAt: currentTime }).returning(['*']);
       return res.status(200).json({
         data: uploadedImage,
         message: 'Image uploaded!'
       })
     } catch(err) {
-      console.log('[controllers][quotation][updateQuotation] :  Error', err);
+      console.log('[controllers][image][uploadImageByEntity] :  Error', err);
+      res.status(500).json({
+        errors: [
+          { code: 'UNKNOWN_SERVER_ERROR', message: err.message }
+        ],
+      });
+    }
+  },
+
+  uploadImageTagsByEntity:async (req,res) => {
+    try {
+      const payload = req.body;
+
+      // validate keys
+      const schema = Joi.object().keys({
+        entityId: Joi.string().required(),
+        entityType: Joi.string().required(),
+        tagData: Joi.object().required(),
+      });
+      // validate params
+      const result = Joi.validate(payload, schema);
+
+      if (result && result.hasOwnProperty("error") && result.error) {
+        console.log("result errors", result);
+        res.status(400).json({
+          errors: [
+            { code: "VALIDATION ERRORS", message: result.message.error }
+          ]
+        });
+      }
+
+      const uploadedImageTags = await knex('image_tags').insert(payload).returning(['*']);
+      return res.status(200).json({
+        data: uploadedImageTags,
+        message: 'Image Tags uploaded!'
+      })
+    } catch(err) {
+      console.log('[controllers][image][uploadImageTagsByEntity] :  Error', err);
       res.status(500).json({
         errors: [
           { code: 'UNKNOWN_SERVER_ERROR', message: err.message }
--- src/routes/image.js
+++ src/routes/image.js
@@ -6,5 +6,6 @@ const authMiddleware = require('../middlewares/auth')
 
 router.post('/delete-image', authMiddleware.isAuthenticated, imageController.deleteImage)
 router.post('/upload-image-by-entity',authMiddleware.isAuthenticated,imageController.uploadImageByEntity)
+router.post('/upload-image-tagg-by-entity',authMiddleware.isAuthenticated,imageController.uploadImageTagsByEntity)
 
 module.exports = router;
