09af9008ae5058117f421d68c64058dba021b75f
Vineet Bhasin <Vineet.Bhasin@grafixsoftware.com>
2022-09-21 17:22:50 +0530

    harvst location sublocation & plants: lotplantlist

--- src/controllers/harvest/masters/get-locations.js
+++ src/controllers/harvest/masters/get-locations.js
@@ -8,6 +8,17 @@ const getLocations = async (req, res) => {
 
         let sqlStr, sqlSelect, sqlFrom, sqlWhere, sqlOrderBy;
 
+        sqlStr = `SELECT DISTINCT l.id, l.name
+        FROM plant_lots pl, plants p, plant_locations pl2, locations l
+        WHERE pl."orgId" = ${orgId} AND pl."companyId" = ${payload.companyId} AND NOT pl."isFinalHarvest"
+        AND pl.id = p."plantLotId" AND p."orgId" = pl2."orgId" AND p.id = pl2."plantId" 
+        AND pl2.id in (SELECT id FROM plant_locations pl3 WHERE pl3."orgId" = pl."orgId" AND pl3."plantId" = p.id order by pl3.id desc limit 1)
+        AND pl2."locationId" IN (${req.GROWINGLOCATION})
+        AND pl2."locationId" = l.id
+        ORDER BY l.name asc
+        `;
+
+/* showing locations that have active (!isFinalHarvest) plant lots
         sqlSelect = `SELECT l.*`;
 
         sqlFrom = ` FROM locations l`;
@@ -19,7 +30,7 @@ const getLocations = async (req, res) => {
         sqlOrderBy = ` ORDER BY l.name asc`;
 
         sqlStr = sqlSelect + sqlFrom + sqlWhere + sqlOrderBy;
-        
+ */        
         var selectedRecs = await knexReader.raw(sqlStr);
 
         return res.status(200).json({
--- src/controllers/harvest/masters/get-sub-locations.js
+++ src/controllers/harvest/masters/get-sub-locations.js
@@ -8,6 +8,17 @@ const getSubLocations = async (req, res) => {
 
         let sqlStr, sqlSelect, sqlFrom, sqlWhere, sqlOrderBy;
 
+        sqlStr = `SELECT DISTINCT sl."locationId", sl.id, sl.name
+        FROM plant_lots pl, plants p, plant_locations pl2, sub_locations sl
+        WHERE pl."orgId" = ${orgId} AND pl."companyId" = ${payload.companyId} AND NOT pl."isFinalHarvest"
+        AND pl.id = p."plantLotId" AND p."orgId" = pl2."orgId" AND p.id = pl2."plantId" 
+        AND pl2.id in (SELECT id FROM plant_locations pl3 WHERE pl3."orgId" = pl."orgId" AND pl3."plantId" = p.id order by pl3.id desc limit 1)
+        AND pl2."locationId" = ${payload.locationId} AND pl2."subLocationId" = sl.id AND pl2."locationId" = sl."locationId"
+        AND pl2."locationId" IN (${req.GROWINGLOCATION})
+        ORDER BY sl.name asc
+        `;
+
+/* showing location's sub locations that have active (!isFinalHarvest) plant lots
         sqlSelect = `SELECT sl.*`;
 
         sqlFrom = ` FROM sub_locations sl`;
@@ -17,6 +28,7 @@ const getSubLocations = async (req, res) => {
         sqlOrderBy = ` ORDER BY sl.name asc`;
 
         sqlStr = sqlSelect + sqlFrom + sqlWhere + sqlOrderBy;
+ */
         
         var selectedRecs = await knexReader.raw(sqlStr);
 
