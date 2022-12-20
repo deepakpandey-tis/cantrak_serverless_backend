62d594acd9f85fcdb5fdeb362444b4a23466e62f
Sukhjinder Arora <sukhjinder@tis.co.th>
2022-12-20 11:45:56 +0700

    added google calendar sync cronjob helper function

---
+++ src/helpers/google-calendar-cron.js
@@ -0,0 +1,57 @@
+const knex = require('../db/knex');
+const knexReader = require('../db/knex-reader');
+
+const googleCalendarCronHelper = async () => {
+    const currentTime = Date.now();
+
+    // Add 7 days to the current date
+    const nextWeekDate = currentTime + 7 * 24 * 60 * 60 * 1000;
+  
+    const workOrdersWithNoEvents = await knexReader('work_plan_schedule_assign_locations')
+      .select(
+        'work_plan_schedule_assign_locations.id', 
+        'work_plan_schedule_assign_locations.orgId', 
+        'google_calendar_events.googleCalEventId'
+      )
+      .leftJoin('google_calendar_events', (qb) => {
+        qb.on('work_plan_schedule_assign_locations.id', '=', 'google_calendar_events.eventEntityId')
+        .andOn('google_calendar_events.eventEntityType', '=', knexReader.raw('?', ['work_order']));
+      })
+      .where('work_plan_schedule_assign_locations.workOrderDate', '>=', currentTime)
+      .andWhere('work_plan_schedule_assign_locations.workOrderDate', '<=', nextWeekDate)
+      .andWhere('google_calendar_events.googleCalEventId', null);
+  
+    /*
+    //Temporary code to test the cronjob with using queueHelper
+    for(let i = 0; i < workOrdersWithNoEvents.length; i++) {
+        const workOrder = workOrdersWithNoEvents[i];
+        setTimeout(async () => {
+            console.log('Adding Event --------------------------------------------------------------')
+            await workOrderEventsHelper
+                .addWorkOrderEvents(+workOrder.id, +workOrder.orgId);
+        }, (i + 1) * 1000)
+    }
+    */
+  
+      const workOrdersChunks = [];
+      const chunkSize = 10;
+  
+      for(let i = 0; i < workOrdersWithNoEvents.length; i += chunkSize) {
+          workOrdersChunks.push(workOrdersWithNoEvents.slice(i, i + chunkSize));
+      }
+  
+      // Import SQS Helper..
+      const queueHelper = require('../helpers/queue');
+  
+      for(const workOrderChunk of workOrdersChunks) {
+          // Using SQS queueHelper to avoid getting rate limiting errors from Google calendar API
+          queueHelper.addToQueue({
+              workOrderChunk: workOrderChunk
+          },
+          'long-jobs',
+          'ADD_WORK_ORDER_CALENDAR_EVENT'
+          ).catch(error => console.log(error));
+      }  
+};
+
+module.exports = googleCalendarCronHelper;
