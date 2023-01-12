d9f7e2d01a7c65150938b817cf08eb5d81cf6407
Vineet Bhasin <Vineet.Bhasin@grafixsoftware.com>
2023-01-12 17:50:14 +0530

    harvest: get-lot-plant-list modified

--- src/controllers/harvest/plants/get-lot-plant-list.js
+++ src/controllers/harvest/plants/get-lot-plant-list.js
@@ -37,6 +37,96 @@ const getLotPlantList = async (req, res) => {
         }
         
         //  getting plant ids which are final harvested
+
+        sqlStr = `SELECT hpl."plantIds" FROM harvest_plant_lots hpl
+        WHERE hpl."orgId" = ${orgId} AND hpl."plantLotId" = ${id} AND hpl."locationId" = ${locationId} AND hpl."subLocationId" = ${subLocationId}
+        AND hpl."isFinalHarvest" AND not hpl."isEntireLot"
+        `;
+
+        var selectedIds = await knexReader.raw(sqlStr);
+        var finalHarvestedPlantIds = [{id: 0}];
+        var plantLotHarvested = false;
+        selectedIds.rows.forEach((el, ndx) => {
+            if(el.plantIds){
+                plantLotHarvested = true;
+                finalHarvestedPlantIds = (ndx == 0) ? [...el.plantIds] : [...finalHarvestedPlantIds, ...el.plantIds];
+            }
+        });
+
+        var finalHarvestedPlantIdsString = finalHarvestedPlantIds.map(r => r?.id);
+        // console.log('finalHarvestedPlantIds: ', finalHarvestedPlantIds);
+        // console.log('finalHarvestedPlantIdsString: ', finalHarvestedPlantIdsString);
+
+        var entireLot = false;
+        if(plantLotHarvested && finalHarvestedPlantIdsString.length == 1 && finalHarvestedPlantIdsString == 0)
+        {
+            entireLot = true;
+        }
+
+        sqlSelect = `SELECT DISTINCT ON (p.id) pl.*, p.id "plantId", p."plantSerial", p."isActive" "plantIsActive", p."isWaste" "plantIsWaste", p."isDestroy" "plantIsDestroy", p."isEndOfLife" "plantIsEndOfLife"
+        , ploc.id "plantLocationId", ploc."locationId" "plantCurrentLocationId", l.name "plantLocationName", ploc."subLocationId" "plantCurrentSubLocationId", sl.name "plantSubLocationName", pgs."growthStageId" "plantGrowthStageId", pgs."startDate" "plantGrowthStageDate", gs.name "plantGrowthStageName"
+        , s.name "strainName", s2.name "specieName"
+        `;
+        sqlSelect += `, observation.*`
+
+        sqlFrom = ` FROM plant_lots pl, plant_locations ploc, plant_growth_stages pgs, locations l, sub_locations sl, growth_stages gs
+        , strains s, species s2, plants p
+        `;
+
+        sqlObservation = `(SELECT DISTINCT ON (i."entityId") it."tagData", i."entityId" FROM images i, image_tags it WHERE i.id = it."entityId" ORDER BY i."entityId" asc, i."createdAt" DESC) observation`;
+        sqlFrom += ` LEFT JOIN ${sqlObservation} ON p.id = observation."entityId"`;
+
+        sqlWhere = ` WHERE pl.id = ${id} AND pl."orgId" = ${orgId} AND pl.id = p."plantLotId" AND p."isActive" AND NOT p."isWaste"`;
+        if(entireLot){
+            // entire lot harvested; show no plant
+            sqlWhere +=  ` AND p.id <= 0`;
+        } else {
+            sqlWhere += ` AND p.id NOT IN (${finalHarvestedPlantIdsString})`;
+        }
+
+        sqlWhere += ` AND p.id = ploc."plantId" AND ploc.id = (select id from plant_locations ploc2 where ploc2."orgId" = ${orgId} and ploc2."plantId" = p.id order by id desc limit 1)`;
+        sqlWhere += ` AND p.id = pgs."plantId" AND pgs.id = (select id from plant_growth_stages pgs2 where pgs2."orgId" = ${orgId} and pgs2."plantId" = p.id order by id desc limit 1)`;
+
+        if(locationId && locationId != ''){
+            sqlWhere += ` AND ploc."locationId" = ${locationId}`;
+        }
+        if(subLocationId){
+            sqlWhere += ` AND ploc."subLocationId" = ${subLocationId}`;
+        }
+
+        if(uptoPlantSerial && uptoPlantSerial != ''){
+            if(fromPlantSerial && fromPlantSerial != ''){
+                sqlWhere += ` AND p."plantSerial" >= '${fromPlantSerial}'`;
+            }
+            sqlWhere += ` AND p."plantSerial" <= '${uptoPlantSerial}'`;
+        }
+        else if(fromPlantSerial && fromPlantSerial != ''){
+            sqlWhere += ` AND p."plantSerial" iLIKE '%${fromPlantSerial}%'`;
+        }
+
+        sqlOrderBy = ` ORDER BY ${sortCol} ${sortOrder}`;
+
+        if(getAllPlants){
+            sqlStr = sqlSelect + sqlFrom + sqlWhere + " ORDER BY p.id desc";
+        }
+        else {
+            //  Paging used
+
+            sqlStr  = `WITH Main_CTE AS (`;
+            sqlStr += sqlSelect + sqlFrom + sqlWhere + `)`;
+            sqlStr += `, Count_CTE AS (SELECT COUNT(*) AS "total" FROM Main_CTE)`;     // To get the total number of records
+            sqlStr += ` SELECT * FROM Main_CTE, Count_CTE`;
+            sqlStr += sqlOrderBy;
+            sqlStr += ` OFFSET ((${pageNumber} - 1) * ${pageSize}) ROWS`
+            sqlStr += ` FETCH NEXT ${pageSize} ROWS ONLY;`;
+
+        }
+
+        //console.log('harvest getLotPlantList: ', sqlStr);
+
+
+
+/*
         sqlStr = `SELECT hpl."plantIds" FROM harvest_plant_lots hpl
         WHERE hpl."plantLotId" = ${id} AND hpl."orgId" = ${orgId} AND hpl."locationId" = ${locationId} AND hpl."subLocationId" = ${subLocationId}
         AND hpl."isFinalHarvest" AND not hpl."isEntireLot"
@@ -51,7 +141,6 @@ const getLotPlantList = async (req, res) => {
         });
         var finalHarvestedPlantIdsString = finalHarvestedPlantIds.map(r => r?.id);
         console.log('finalHarvestedPlantIdsString: ', finalHarvestedPlantIdsString);
-
 
         // Using CTE (Common Table Expressions 'SELECT in WITH' for pageSize retrieval)
         sqlSelect = `SELECT pl.*, p.id "plantId", p."plantSerial", p."isActive" "plantIsActive", p."isWaste" "plantIsWaste", p."isDestroy" "plantIsDestroy", p."isEndOfLife" "plantIsEndOfLife"
@@ -87,17 +176,6 @@ const getLotPlantList = async (req, res) => {
             sqlWhere += ` AND p."plantSerial" iLIKE '%${fromPlantSerial}%'`;
         }
 
-
-/*         if(fromDate && fromDate != ''){
-            sdt = new Date(fromDate).getTime()
-            sqlWhere += ` AND p2."plantedOn" >= ${sdt}`;
-        }
-
-        if(endDate && endDate != ''){
-            edt = new Date(endDate).getTime()
-            sqlWhere += ` AND p2."plantedOn" <= ${edt}`;
-        }
- */
         sqlOrderBy = ` ORDER BY ${sortCol} ${sortOrder}`;
         //console.log('getLotPlantList sql: ', sqlSelect + sqlFrom + sqlWhere);
 
@@ -116,8 +194,7 @@ const getLotPlantList = async (req, res) => {
             sqlStr += ` FETCH NEXT ${pageSize} ROWS ONLY;`;
 
         }
-
-        //console.log('getLotPlantList: ', sqlStr);
+        */
         
         var selectedRecs = await knexReader.raw(sqlStr);
         //console.log('selectedRecs: ', selectedRecs);
